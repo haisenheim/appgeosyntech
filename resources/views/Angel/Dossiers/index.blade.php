24da085d784c5caad082681e946f8b717a1264b7
Clement <clementessomba@gmail.com>
2019-10-06 16:20:56 +0100

    prmiere sauvegarde de Makokou - samedi le 05/10/19 8h:40

--- resources/views/Angel/Dossiers/index.blade.php
+++ resources/views/Angel/Dossiers/index.blade.php
@@ -19,7 +19,7 @@
                     <div class="tab-content" id="custom-content-below-tabContent">
                       <div class="tab-pane fade show active" id="custom-content-below-home" role="tabpanel" aria-labelledby="custom-content-below-home-tab">
                         <div class="row">
-                        <?php dd($projets) ?>
+
                             @foreach($projets as $dossier)
                                  <div class="col-md-4 col-sm-12">
                                             <!-- Widget: user widget style 1 -->
