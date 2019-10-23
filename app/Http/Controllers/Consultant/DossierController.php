4c83f0582bade1f8a79a098fcfc77bf26afa3ea1
Clement <clementessomba@gmail.com>
2019-10-23 12:14:37 +0100

    Petits changements

--- app/Http/Controllers/Consultant/DossierController.php
+++ app/Http/Controllers/Consultant/DossierController.php
@@ -31,7 +31,7 @@ class DossierController extends Controller
     public function index()
     {
         //
-        $dossiers = Projet::all()->where('expert_id',Auth::user()->id);
+        $dossiers = Projet::all()->where('expert_id',Auth::user()->id)->paginate(2);
         return view('/Consultant/Dossiers/index')->with(compact('dossiers'));
     }
 
