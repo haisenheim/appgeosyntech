0bee765804fb03ddc2b13eb463f8431c0159761d
Clement <clementessomba@gmail.com>
2019-10-23 12:09:42 +0100

    Petits changements

--- app/Http/Controllers/Consultant/DashboardController.php
+++ app/Http/Controllers/Consultant/DashboardController.php
@@ -9,6 +9,6 @@ class DashboardController extends Controller
 {
     public function __invoke()
     {
-        return view('/consultant/dashboard');
+        return view('Consultant/dashboard');
     }
 }
