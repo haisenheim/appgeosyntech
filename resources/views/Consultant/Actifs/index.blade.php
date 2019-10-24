86ccd6f57fa265573fde6a06a924eddb713eadd5
Clement <clementessomba@gmail.com>
2019-10-24 14:06:35 +0100

    Petits changements

--- resources/views/Consultant/Actifs/index.blade.php
+++ resources/views/Consultant/Actifs/index.blade.php
@@ -39,8 +39,11 @@
                               <!-- Add the bg color to the header using any of the bg-* classes -->
                               <div class="widget-user-header text-white"
                                    style="background: url('{{ $projet->imageUri?asset('img/'.$projet->imageUri):asset('img/logo.png') }}') center center;">
-                                    <h3 style="font-weight: 900; background: #EEEEEE; opacity: 0.5" class="widget-user-username text-right"><?= $projet->name ?></h3>
-                                    <h5 style="font-weight: 700; background: #EEEEEE" class="widget-user-desc text-right">{{ $projet->owner->name }}</h5>
+                                   <div style="background: #EEEEEE; opacity: 0.5">
+                                   <h3 style="font-weight: 900;" class="widget-user-username text-right"><?= $projet->name ?></h3>
+                                    <h5 style="font-weight: 700;" class="widget-user-desc text-right">{{ $projet->owner->name }}</h5>
+                                   </div>
+
                               </div>
                               <div class="widget-user-image">
                                 <img class="img-circle" src="{{$projet->user?$projet->user->imageUri? asset('img/'.$projet->user->imageUri):asset('img/avatar.png'):asset('img/avatar.png')}}" alt="User Avatar">
