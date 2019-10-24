2b65d17adba5a78c9be5761dffb0ce51258b3576
Clement <clementessomba@gmail.com>
2019-10-24 12:50:46 +0100

    Petits changements

--- resources/views/Consultant/Dossiers/show.blade.php
+++ resources/views/Consultant/Dossiers/show.blade.php
@@ -8,7 +8,7 @@
 @section('content')
     <div style="padding-top: 30px; padding-bottom: 80px;" class="container-fluid">
                 <div class="row">
-                    <div id="side1" class="col-md-4 col-sm-12" style="min-height:840px ">
+                    <div id="side1" class="col-md-4 col-sm-12" style="max-height:840px ">
                         <div class="card">
                             <div class="card-body">
 
@@ -120,7 +120,7 @@
 
 
                     </div>
-                    <div style="overflow-y: scroll; min-height: 840px" id="side2" class="col-md-8 col-sm-12">
+                    <div style="overflow-y: scroll; max-height: 840px" id="side2" class="col-md-8 col-sm-12">
                         <div class="card">
                             <div class="card-body">
                                <fieldset>
