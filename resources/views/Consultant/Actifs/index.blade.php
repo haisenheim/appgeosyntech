c1395e2990b251d6bb1d459e83bc57416735d063
Clement <clementessomba@gmail.com>
2019-10-24 14:14:22 +0100

    Petits changements

--- resources/views/Consultant/Actifs/index.blade.php
+++ resources/views/Consultant/Actifs/index.blade.php
@@ -9,30 +9,9 @@
 
                 <div class="row">
                     @foreach($dossiers as $projet)
-                    <div class="col-sm-12 col-md-3">
-                    <div class="widget widget-success">
-                        <div style="padding-left: 10px" class="widget-header">
-                             <h5 class="widget-title">{{ $projet->name }}</h5>
-                        </div>
-                        <div class="widget-content">
-                            <div class="">
-                                <div class="">
-                                    <span style="font-weight: 600; font-size: 1.3rem" class="text-primary"><i class="fa fa-th-large"></i> {{  $projet->tactif->name }}</span>
-                                </div>
-                                <div class="text text-danger text-bold"> <i class="fa fa-credit-card"></i> {{ $projet->prix }} </div>
-                                <div class="">
-                                    <span class=""><i class="fa fa-user"></i> {{  $projet->owner->name }}</span>
-                                </div>
-                            </div>
-                            <div>
-                                <span class=""><i class="fa fa-map-marker"></i> {{ $projet->ville->name  }}</span>
-                                <span class="pull-right"><a class="btn btn-success btn-xs" href="/consultant/actifs/{{ $projet->token  }}"> Afficher </a></span>
-                            </div>
-                        </div>
-                    </div>
-                     </div>
 
-                     <div class="col-md-3">
+
+                     <div class="col-md-3 col-sm-12">
             <!-- Widget: user widget style 1 -->
                         <a style="color:#555" href="/consultant/actifs/{{ $projet->token  }}">
                             <div class="card card-widget widget-user">
