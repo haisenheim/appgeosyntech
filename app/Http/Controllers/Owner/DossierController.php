b9f7b2bfc12de5211f6201b763562d6d4b34949b
Clement <clementessomba@gmail.com>
2019-10-24 15:49:46 +0100

    Petits changements

--- app/Http/Controllers/Owner/DossierController.php
+++ app/Http/Controllers/Owner/DossierController.php
@@ -30,7 +30,7 @@ class DossierController extends Controller
     public function index()
     {
         //
-        $dossiers = Projet::orderBy('created_at','desc')->where('owner_id',Auth::user()->id)->paginate(12);
+        $dossiers = Projet::orderBy('created_at','desc')->where('owner_id',Auth::user()->id)->paginate(8);
         return view('/Owner/Dossiers/index')->with(compact('dossiers'));
     }
 
