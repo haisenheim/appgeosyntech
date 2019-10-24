820918849a7fe9f0cecb23ec4f84623f6f2092e7
Clement <clementessomba@gmail.com>
2019-10-24 16:18:03 +0100

    Petits changements

--- resources/views/Owner/Actifs/index.blade.php
+++ resources/views/Owner/Actifs/index.blade.php
@@ -1,33 +1,62 @@
 @extends('......layouts.owner')
 
+@section('content-header')
+     <h3 style="font-weight: 800; margin-top: 50px; color: #FFFFFF; padding-bottom: 15px; border-bottom: solid #FFFFFF 1px;" class="page-header">GESTION DES CESSIONS D'ACTIFS</h3>
+@endsection
+
 @section('content')
     <div style="padding-top: 30px" class="container-fluid">
 
                 <div class="row">
                     @foreach($dossiers as $projet)
-                    <div class="col-sm-12 col-md-3">
-                    <div class="widget widget-success">
-                        <div style="padding-left: 10px" class="widget-header">
-                             <h5 class="widget-title">{{ $projet->name }}</h5>
-                        </div>
-                        <div class="widget-content">
-                            <div class="">
-                                <div class="">
-                                    <span style="font-weight: 600; font-size: 1.3rem" class="text-primary"><i class="fa fa-th-large"></i> {{  $projet->tactif->name }}</span>
+
+
+                     <div class="col-md-3 col-sm-12">
+            <!-- Widget: user widget style 1 -->
+                        <a style="color:#555" href="/consultant/actifs/{{ $projet->token  }}">
+                            <div class="card card-widget widget-user">
+                              <!-- Add the bg color to the header using any of the bg-* classes -->
+                              <div class="widget-user-header text-white"
+                                   style="background: url('{{ $projet->imageUri?asset('img/'.$projet->imageUri):asset('img/logo.png') }}') center center;">
+                                   <div style="background: #EEEEEE;">
+                                   <h3 style="font-weight:900; color: #4caf50;" class="widget-user-username text-right"><?= $projet->name ?></h3>
+                                    <h5 style="font-weight: 700;color: #4caf50;" class="widget-user-desc text-right">{{ $projet->owner->name }}</h5>
+                                   </div>
+
+                              </div>
+                              <div class="widget-user-image">
+                                <img class="img-circle" src="{{$projet->user?$projet->user->imageUri? asset('img/'.$projet->user->imageUri):asset('img/avatar.png'):asset('img/avatar.png')}}" alt="User Avatar">
+                              </div>
+                              <div style="padding: .75rem 1.25rem;" class="card-footer">
+                                <div class="row">
+                                    <div class="col-md-6 border-right">
+                                        <div class="description-block">
+                                          <h5 class="description-header"><i class="fa fa-map-marker"></i></h5>
+                                          <span class="description-text">{{ $projet->ville->name  }}</span>
+                                        </div>
+                                    </div>
+
+                                  <!-- /.col -->
+                                  <div class="col-sm-6">
+                                    <div class="description-block">
+                                      <h5 class="description-header"><i class="fa fa-coins"></i></h5>
+                                      <span class="description-text">{{$projet->prix}} FCFA</span>
+                                    </div>
+                                    <!-- /.description-block -->
+                                  </div>
+                                  <!-- /.col -->
                                 </div>
-                                <div class="text text-danger text-bold"> <i class="fa fa-credit-card"></i> {{ $projet->prix }} </div>
-                                <div class="">
-                                    <span class=""><i class="fa fa-user"></i> {{  $projet->owner->name }}</span>
-                                </div>
+                                <!-- /.row -->
+                              </div>
                             </div>
-                            <div>
-                                <span class=""><i class="fa fa-map-marker"></i> {{ $projet->ville->name  }}</span>
-                                <span class="pull-right"><a class="btn btn-success btn-xs" href="/owner/actifs/{{ $projet->token  }}"> Afficher </a></span>
-                            </div>
-                        </div>
+                        </a>
+
+            <!-- /.widget-user -->
                     </div>
-                     </div>
+
                     @endforeach
+
+
 
                 </div>
             </div>
@@ -36,5 +65,4 @@
 
 @section('action')
 
-<a class="btn btn-sm btn-primary" href="/owner/actifs/create"><i class="fa fa-plus-circle"></i> NOUVELLE CESSION</a>
 @endsection
--- resources/views/Owner/Actifs/show.blade.php
+++ resources/views/Owner/Actifs/show.blade.php
@@ -1,23 +1,25 @@
 @extends('......layouts.owner')
+
+@section('content-header')
+    <div class="">
+       <h3 style="font-weight: 800; margin-top: 50px; color: #FFFFFF; padding-bottom: 15px; border-bottom: solid #FFFFFF 1px;" class="page-header">GESTION DES CESSIONS D'ACTIFS</h3>
+        <h5 style="font-weight: 600; margin-top: 20px; color: #FFFFFF; padding-bottom: 5px; border-bottom: solid #FFFFFF 1px;"> {{$projet->name}}</h5>
+    </div>
+@endsection
+
 @section('content')
     <div style="padding-top: 30px" class="container-fluid">
                 <div class="row">
 
                     <div class="col-md-4 col-sm-12">
-                        <div class="well">
+                        <div class="card">
+                            <div class="card-body">
 
-                            <h4 style="background-color: inherit">{{ $projet->name  }}</h4>
+                                <a data-toggle="modal" data-target="#uploadImgModal" href="" title="modifier l'image"><i class="fa fa-pencil"></i>
+                                    <div style="height: 300px; width: 100%; background: url('{{ $projet->imageUri?asset('img/'.$projet->imageUri):asset('img/logo.png') }}'); background-size: cover ">
 
-                            <div style="max-height: 300px; max-width: 300px">
-                                @if($projet->imageUri)
-                                    <img class="img-thumbnail" src="{{asset('img/'.$projet->imageUri)}}" alt=""/>
-                                    <a data-toggle="modal" data-target="#uploadImgModal" href="" title="modifier l'image"><i class="fa fa-pencil"></i></a>
-                                @else
-                                     <img class="img-thumbnail" src="{{asset('img/logo-obac.png')}}" alt=""/>
-                                     <a data-toggle="modal" data-target="#uploadImgModal" href="" title="modifier l'image"><i class="fa fa-pencil"></i></a>
-                                @endif
-                            </div>
-
+                                    </div>
+                                </a>
                             <p>TYPE D'IMMOBILISATION : {{ $projet->tactif->name }}</p>
                             <p>DATE DE CREATION : <span class="value"> {{ date_format($projet->created_at,'d/m/Y') }}</span></p>
                             <p>PROMOTEUR : <span class="value">{{ $projet->owner->name }}</span></p>
@@ -35,224 +37,88 @@
 
 
                         </div>
-
+                        </div>
 
                     </div>
                     <div class="col-md-8 col-sm-12">
-                        <div class="widget">
-                            <div class="widget-content">
-                               <fieldset>
-                                    <legend>DESCRIPTION</legend>
+                        <div class="card">
+                            <div class="card-header">
+
+                                    <h5>DESCRIPTION</h5>
+                            </div>
+                            <div class="card-body">
                                     <p>
                                         <?= $projet->description ?>
                                     </p>
-                               </fieldset>
-                               <hr/>
-                               <fieldset>
-                                    <legend>CARACTERISTIQUES</legend>
+
+                               </div>
+                        </div>
+
+
+                        <div class="card">
+                               <div class="card-header">
+                                    <h5>CARACTERISTIQUES</h5>
+                               </div>
+                               <div class="card-body">
                                     <p>
                                         <?= $projet->caracteristiques ?>
                                     </p>
-                               </fieldset>
+                               </div>
                             </div>
+                    </div>
                         </div>
                     </div>
-                </div>
-
-    </div>
 
 
+      <div class="modal fade modal-lg" id="TModal" tabindex="-1" role="dialog" aria-labelledby="addModalLabel">
+            <div class="modal-header">
+                <h5>EDITION DU PITCH</h5>
+            </div>
+            <div class="modal-content">
+                <form class="form" action="/consultant/actif/save"  method="post">
+                    {{csrf_field()}}
+                    <input type="hidden" name="token" value="{{$projet->token}}"/>
+                    <input type="hidden" name="id" value="{{$projet->id}}"/>
 
-        <div class="modal fade" id="addTagModal" tabindex="-1" role="dialog" aria-labelledby="addModalLabel">
-                	<form enctype="multipart/form-data" method="post" action="/owner/actif/add-tag">
-                		<input type="hidden" id="" name="projet_token" value="<?= $projet->token ?>" />
-                		{{csrf_field()}}
-                		<div class="modal-dialog modal-lg" role="document">
-                			<div class="modal-content">
-                				<div class="modal-header">
-                					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                					<h5 style="text-transform: uppercase; background-color: transparent" class="modal-title" id="myModalLabel"><span> Chargement de l'image du projet</span></h5>
-                				</div>
-                				<div class="modal-body">
-                					<div class="form-group">
-                						 <select class="form-control" name="tag_id" id="tag_id">
-                                            @foreach($tags as $tag)
-                                                <option value="{{$tag->id}}">{{$tag->name}}</option>
-                                            @endforeach
-                                          </select>
-                					</div>
-                				</div>
-                				<div class="modal-footer">
-                					<button type="submit" class="btn btn-danger"><i class="fa fa-save"></i> ENREGISTRER</button>
-                				</div>
+                    <fieldset>
+                        <legend>EDITION DU PITCH</legend>
+                            <div class="row">
 
-                			</div>
-                		</div>
-                	</form>
+                                 <div class="col-md-12 col-sm-12">
+                                     <div class="form-group">
+                                         <label for="description" class="control-label">TEASER</label>
+                                         <textarea name="teaser" class="form-control" id="description" cols="30" rows="3"><?= $projet->teaser ?></textarea>
+                                     </div>
+                                 </div>
 
+                             </div>
 
+                             <div class="btn-div card-footer text-center">
+                                 <button class="btn btn-success  btn-sm " type="submit"> Enregistrer <i class="fa fa-save"></i></button>
+                            </div>
 
-                </div>
+                     </fieldset>
+                </form>
+            </div>
+      </div>
 
+      <script type="text/javascript" src="{{ asset('summernote/dist/summernote.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('summernote/lang/summernote-fr-FR.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('summernote/dist/summernote.css') }}"/>
 
-    <script>
-        $(document).ready(function(){
-            $('.fa-modif').css({
-            cursor:'pointer',
-            display:'none'
-            });
-            getPlan($('#plan_id').val());
-            //var orm = 'http://localhost/ormsys/api/';
-            $.ajax({
-                url: "/owner/dossier/getchoices",
-                type:'Get',
-                dataType:'json',
-                data:{id:$('#id').val()},
-                success:function(data){
-                    if(data!=null){
-                        $.ajax({
-                            url:orm+'carto',
-                            type:'Post',
-                            dataType:'json',
-                            data:{choix:data},
-                            success:function(rep){
-                                $('#risks-loader').hide();
-
-                                var html = '';
-                                //console.log(Object.entries(rep));
-                                var risks=Object.entries(rep);
-                                for(var i=0; i<risks.length;i++){
-
-                                    var rs= parseInt(risks[i][1].length) + 1;
-                                    var tr= '<tr><th style="align-content: center; margin-top: auto" align="center" rowspan='+ rs  +'>'+ risks[i][0] +'</th></tr>';
-                                    html=html+tr;
-                                    for(var k=0; k<risks[i][1].length; k++){
-                                        $value = risks[i][1][k];
-                                        $cb= parseInt($value.question.produits_risque.frequence) * parseInt($value.question.produits_risque.gravite);
-                                        $cn=parseInt($value.question.produits_risque.frequence) * parseInt($value.question.produits_risque.gravite) * parseFloat($value.taux);
-
-                                        if(parseFloat($cb) >= 13){
-                                            $clrb='red';
-                                        }else{
-                                            if( parseFloat($cb) >=4 && parseFloat($cb) <= 12){
-                                                $clrb='yellow';
-                                            }else{
-                                                $clrb = '#0ac60a';
-                                            }
-                                        }
-
-                                        if( parseFloat($cn) >= 13){
-                                            $clr='red';
-                                        }else{
-                                            if( parseFloat($cn) >=4 &&  parseFloat($cn) <= 12){
-                                                $clr='yellow';
-                                            }else{
-                                                $clr = '#0ac60a';
-                                            }
-                                        }
-
-                                        var trr = '<tr>'+
-                                            '<td>'+ $value.question.produits_risque.name +'</td>'+
-                                            '<td>'+$value.question.produits_risque.causes +'</td>'+
-                                            '<td>'+ $value.question.produits_risque.consequences +'</td>'+
-                                            '<td>'+ $value.question.produits_risque.frequence +'</td>'+
-                                            '<td>'+ $value.question.produits_risque.gravite+'</td>'+
-                                            '<td style="background-color:'+ $clrb +'; font-weight: 900; text-align: right">'+ $cb  +'</td>'+
-                                            '<td style="background-color:'+ $clr +'">'+ $cn +'</td>'+
-                                        '</tr>';
-
-                                        html=html+trr;
-
-                                        //console.log(risks[i][1][k]);
-                                    }
-                                    //console.log(risks[i][1]);
-
-                                }
-
-                                $('#risques-tab').find('tbody').html(html);
-                            },
-                            Error:function(){
-                                $('#risks-loader').hide();
-                            }
-                        });
-                    }
-                }
-            })
+    <script type="text/javascript">
+        $(document).ready(function() {
+          $('textarea').summernote({
+            height: 200,
+            tabsize: 2,
+            followingToolbar: true,
+            lang:'fr-FR'
+          });
         });
-
-        function getPlan(id){
-
-                         $.ajax({
-                           url:orm+'get-plan',
-                           type:'Get',
-                           dataType:'json',
-                           data:{id:id},
-                               success:function(data){
-                                   //console.log(data);
-                                   if(data!=null){
-                                        $.ajax({
-                                          url:orm+'get-plan',
-                                          type:'Get',
-                                          dataType:'json',
-                                          data:{id:id},
-                                        success:function(){
-                                        }
-                                        });
-                                   }
-                                   var html = '';
-                                   var pls = data.plignes;
-                                   for(var i = 0; i<data.plignes.length; i++){
-
-                                        var tr ='<tr data-id="'+ pls[i].id +'"><td style="width: 13%">'+ pls[i].produits_risque.risque.name +'</td><td style="width: 20%">'+ pls[i].produits_risque.produit.name +'</td><td style="width: 20%">'+ pls[i].produits_risque.name +'</td><td contenteditable="true" style="width: 37%">'+ pls[i].amelioration +'</td></tr>';
-                                        html = html + tr;
-                                   }
-                                   $('#example').find('tbody').html(html);
-                               },
-                           Error:function(){
-
-                           }
-                         });
-
-                         $('.td-modif').hover(function(){
-                            $(this).find('.fa-modif').css({
-                            display:'block',
-                            margin:'-15px 0 0 20px',
-                            'z-index':99999,
-                            'text-align':'right'
-                            }).click(function(e){
-                                $(this).parent('td').prop('contentEditable',true)
-                            })
-                         }).mouseleave(function(){
-                            $(this).find('.fa-modif').css({display:'none'});
-                         })
-
-                         $('.td-modif').keypress(function(e){
-                            //console.log(e);
-                            if($(this).prop('contentEditable')){
-                                //console.log(e.keyCode);
-                                if(e.keyCode==13){
-                                    var name = $(this).data('name');
-                                    var model = $(this).data('model');
-                                    var value = $(this).text();
-                                    var id = $(this).data('id');
-                                    $.ajax({
-                                        url:'/owner/dossier/edit-field',
-                                        type:'get',
-                                        dataType:'json',
-                                        data:{name:name,model:model,value:value,id:id},
-                                        success:function(data){
-                                            window.location.reload();
-                                        }
-                                    })
-                                }
-                                //console.log($(this).data('name'));
-                            }
-                         });
-                    }
-    </script>
+      </script>
 
 @endsection
 
 @section('action')
-     <a class="btn btn-xs btn-success" href="/owner/actifs/{{$projet->token}}/edit"><i class="fa fa-pencil"></i> Modifier</a>
+     <a class="btn btn-xs btn-success" href="#" data-target="#TModal" data-toggle="modal"><i class="fa fa-pencil"></i> Editer le teaser</a>
 @endsection
