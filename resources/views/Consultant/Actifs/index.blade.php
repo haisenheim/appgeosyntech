7907b69f0666c4e44c9af17b0bd0cd13498f648d
Clement <clementessomba@gmail.com>
2019-10-24 14:02:23 +0100

    Petits changements

--- resources/views/Consultant/Actifs/index.blade.php
+++ resources/views/Consultant/Actifs/index.blade.php
@@ -31,52 +31,50 @@
                         </div>
                     </div>
                      </div>
+
+                     <div class="col-md-3">
+            <!-- Widget: user widget style 1 -->
+                        <a style="color:#555" href="/consultant/actifs/{{ $projet->token  }}">
+                            <div class="card card-widget widget-user">
+                              <!-- Add the bg color to the header using any of the bg-* classes -->
+                              <div class="widget-user-header text-white"
+                                   style="background: url('{{ $projet->imageUri?asset('img/'.$projet->imageUri):asset('img/logo.png') }}') center center;">
+                                    <h3 style="font-weight: 900" class="widget-user-username text-right"><?= $projet->name ?></h3>
+                                    <h5 style="font-weight: 700" class="widget-user-desc text-right">{{ $projet->owner->name }}</h5>
+                              </div>
+                              <div class="widget-user-image">
+                                <img class="img-circle" src="{{$projet->user?$projet->user->imageUri? asset('img/'.$projet->user->imageUri):asset('img/avatar.png'):asset('img/avatar.png')}}" alt="User Avatar">
+                              </div>
+                              <div style="padding: .75rem 1.25rem;" class="card-footer">
+                                <div class="row">
+                                    <div class="col-md-6 border-right">
+                                        <div class="description-block">
+                                          <h5 class="description-header"><i class="fa fa-map-marker"></i></h5>
+                                          <span class="description-text">{{ $projet->ville->name  }}</span>
+                                        </div>
+                                    </div>
+
+                                  <!-- /.col -->
+                                  <div class="col-sm-6">
+                                    <div class="description-block">
+                                      <h5 class="description-header"><i class="fa fa-coins"></i></h5>
+                                      <span class="description-text">{{$projet->montant}} FCFA</span>
+                                    </div>
+                                    <!-- /.description-block -->
+                                  </div>
+                                  <!-- /.col -->
+                                </div>
+                                <!-- /.row -->
+                              </div>
+                            </div>
+                        </a>
+
+            <!-- /.widget-user -->
+                    </div>
+
                     @endforeach
 
-            <div class="col-md-3">
-            <!-- Widget: user widget style 1 -->
-            <div class="card card-widget widget-user">
-              <!-- Add the bg color to the header using any of the bg-* classes -->
-              <div class="widget-user-header text-white"
-                   style="background: url('../dist/img/photo1.png') center center;">
-                <h3 class="widget-user-username text-right">Elizabeth Pierce</h3>
-                <h5 class="widget-user-desc text-right">Web Designer</h5>
-              </div>
-              <div class="widget-user-image">
-                <img class="img-circle" src="../dist/img/user3-128x128.jpg" alt="User Avatar">
-              </div>
-              <div class="card-footer">
-                <div class="row">
-                  <div class="col-sm-4 border-right">
-                    <div class="description-block">
-                      <h5 class="description-header">3,200</h5>
-                      <span class="description-text">SALES</span>
-                    </div>
-                    <!-- /.description-block -->
-                  </div>
-                  <!-- /.col -->
-                  <div class="col-sm-4 border-right">
-                    <div class="description-block">
-                      <h5 class="description-header">13,000</h5>
-                      <span class="description-text">FOLLOWERS</span>
-                    </div>
-                    <!-- /.description-block -->
-                  </div>
-                  <!-- /.col -->
-                  <div class="col-sm-4">
-                    <div class="description-block">
-                      <h5 class="description-header">35</h5>
-                      <span class="description-text">PRODUCTS</span>
-                    </div>
-                    <!-- /.description-block -->
-                  </div>
-                  <!-- /.col -->
-                </div>
-                <!-- /.row -->
-              </div>
-            </div>
-            <!-- /.widget-user -->
-          </div>
+
 
                 </div>
             </div>
