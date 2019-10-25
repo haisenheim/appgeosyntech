888fa58090f3ebea782973109937aad3391e81dc
Clement <clementessomba@gmail.com>
2019-10-25 07:08:08 +0100

    Petits changements

--- resources/views/Owner/Actifs/show.blade.php
+++ resources/views/Owner/Actifs/show.blade.php
@@ -10,21 +10,15 @@
 @section('content')
     <div style="padding-top: 30px" class="container-fluid">
                 <div class="row">
-
                     <div class="col-md-4 col-sm-12">
                         <div class="card">
                             <div class="card-body">
-
-                                <a data-toggle="modal" data-target="#uploadImgModal" href="" title="modifier l'image"><i class="fa fa-pencil"></i>
                                     <div style="height: 300px; width: 100%; background: url('{{ $projet->imageUri?asset('img/'.$projet->imageUri):asset('img/logo.png') }}'); background-size: cover ">
 
                                     </div>
-                                </a>
                             <p>TYPE D'IMMOBILISATION : {{ $projet->tactif->name }}</p>
                             <p>DATE DE CREATION : <span class="value"> {{ date_format($projet->created_at,'d/m/Y') }}</span></p>
                             <p>PROMOTEUR : <span class="value">{{ $projet->owner->name }}</span></p>
-
-
 
                             <p>PRIX INITIAL : {{ $projet->prix }}</p>
                             <input type="hidden" id="id" value="<?= $projet->id ?>"/>
@@ -34,7 +28,107 @@
 
                             @endif
 
+                            @if($projet->cessions)
+                                <h5>LISTE DES CANDIDATS</h5>
+                                <ul class="list-group">
+                                    @foreach($projet->cessions as $cession)
+                                        <li class="list-group-item">
+                                            <a href="#" class="link-black" data-target="#ShowComment" data-toggle="modal">
 
+                                                <div class="card card-prirary cardutline direct-chat direct-chat-primary">
+                                                              <div class="card-header">
+                                                                <h3 class="card-title">{{$cession->angel->name}}</h3>
+
+                                                                <div class="card-tools">
+                                                                  <span data-toggle="tooltip" title="3 New Messages" class="badge bg-primary">3</span>
+                                                                  <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
+                                                                  </button>
+                                                                  <button type="button" class="btn btn-tool" data-toggle="tooltip" title="Contacts"
+                                                                          data-widget="chat-pane-toggle">
+                                                                    <i class="fas fa-comments"></i></button>
+                                                                  <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i>
+                                                                  </button>
+                                                                </div>
+                                                              </div>
+                                                              <!-- /.card-header -->
+                                                              <div class="card-body">
+                                                                <!-- Conversations are loaded here -->
+                                                                <div class="direct-chat-messages">
+                                                                  <!-- Message. Default to the left -->
+                                                                  <div class="direct-chat-msg">
+                                                                    <div class="direct-chat-infos clearfix">
+                                                                      <span class="direct-chat-name float-left">Alexander Pierce</span>
+                                                                      <span class="direct-chat-timestamp float-right">23 Jan 2:00 pm</span>
+                                                                    </div>
+                                                                    <!-- /.direct-chat-infos -->
+                                                                    <img class="direct-chat-img" src="../dist/img/user1-128x128.jpg" alt="Message User Image">
+                                                                    <!-- /.direct-chat-img -->
+                                                                    <div class="direct-chat-text">
+                                                                      Is this template really for free? That's unbelievable!
+                                                                    </div>
+                                                                    <!-- /.direct-chat-text -->
+                                                                  </div>
+                                                                  <!-- /.direct-chat-msg -->
+
+                                                                  <!-- Message to the right -->
+                                                                  <div class="direct-chat-msg right">
+                                                                    <div class="direct-chat-infos clearfix">
+                                                                      <span class="direct-chat-name float-right">Sarah Bullock</span>
+                                                                      <span class="direct-chat-timestamp float-left">23 Jan 2:05 pm</span>
+                                                                    </div>
+                                                                    <!-- /.direct-chat-infos -->
+                                                                    <img class="direct-chat-img" src="../dist/img/user3-128x128.jpg" alt="Message User Image">
+                                                                    <!-- /.direct-chat-img -->
+                                                                    <div class="direct-chat-text">
+                                                                      You better believe it!
+                                                                    </div>
+                                                                    <!-- /.direct-chat-text -->
+                                                                  </div>
+                                                                  <!-- /.direct-chat-msg -->
+                                                                </div>
+                                                                <!--/.direct-chat-messages-->
+
+                                                                <!-- Contacts are loaded here -->
+                                                                <div class="direct-chat-contacts">
+                                                                  <ul class="contacts-list">
+                                                                    <li>
+                                                                      <a href="#">
+                                                                        <img class="contacts-list-img" src="../dist/img/user1-128x128.jpg">
+
+                                                                        <div class="contacts-list-info">
+                                                                          <span class="contacts-list-name">
+                                                                            Count Dracula
+                                                                            <small class="contacts-list-date float-right">2/28/2015</small>
+                                                                          </span>
+                                                                          <span class="contacts-list-msg">How have you been? I was...</span>
+                                                                        </div>
+                                                                        <!-- /.contacts-list-info -->
+                                                                      </a>
+                                                                    </li>
+                                                                    <!-- End Contact Item -->
+                                                                  </ul>
+                                                                  <!-- /.contatcts-list -->
+                                                                </div>
+                                                                <!-- /.direct-chat-pane -->
+                                                              </div>
+                                                              <!-- /.card-body -->
+                                                              <div class="card-footer">
+                                                                <form action="#" method="post">
+                                                                  <div class="input-group">
+                                                                    <input type="text" name="message" placeholder="Type Message ..." class="form-control">
+                                                                    <span class="input-group-append">
+                                                                      <button type="submit" class="btn btn-primary">Send</button>
+                                                                    </span>
+                                                                  </div>
+                                                                </form>
+                                                              </div>
+                                                              <!-- /.card-footer-->
+                                                            </div>
+                                            </a>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            @endif
 
                         </div>
                         </div>
@@ -87,7 +181,7 @@
                 <div class="modal-content">
                     <div class="modal-header">
                 <h4 class="modal-title">{{ $projet->name }}</h4>
-                <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">X</span></button>
+                <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">x</span></button>
             </div>
             <div class="modal-body">
                  <form enctype="multipart/form-data" class="form" action="{{route('owner.update.actif')}}"  method="post">
@@ -192,7 +286,7 @@
              @endif
              <div class="dropdown-divider"></div>
              <a href="#" data-target="#EditModal" data-toggle="modal" class="dropdown-item">
-               <i class="fas fa-pencil mr-2"></i> Modifier
+               <i class="fa fa-pencil mr-2"></i> Modifier
              </a>
              <div class="dropdown-divider"></div>
              <a href="#" class="dropdown-item">
