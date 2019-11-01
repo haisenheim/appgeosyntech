3d2a932f35c1a8e6718a3b4b65069e75abdc25a2
Clement <clementessomba@gmail.com>
2019-11-01 10:58:57 +0100

    petits changements

--- resources/views/layouts/angel.blade.php
+++ resources/views/layouts/angel.blade.php
@@ -121,10 +121,10 @@
                         <div style="min-height: 300px; background: url('{{$slides['actif']->imageUri? asset('img/'.$slides['actif']->imageUri):asset('img/logo-obac.png')}}'); background-size: cover">
                             <div style="width: 800px; margin: 100px auto; " class="carousel-content">
 
-                                <div style="width: 800px; padding: 10px auto; " class="actif-description bg-info card">
+                                <div style="width: 800px; padding: 10px auto; " class="actif-description card">
                                     <div class="card-body">
                                         <h3>{{ $slides['actif']->name }}</h3>
-                                        <a class="btn btn-primary btn-sm" href="/angel/actif/{{$slides['actif']->token}}">Consulter</a>
+                                        <a class="btn btn-primary btn-sm" href="/angel/actifs/{{$slides['actif']->token}}">Consulter</a>
                                     </div>
 
                                 </div>
