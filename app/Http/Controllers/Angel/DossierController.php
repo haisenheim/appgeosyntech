4a0eeae8dbf6689a672d0d5de1940f34185cf3b4
Clement <clementessomba@gmail.com>
2019-11-01 12:42:31 +0100

    petits changements

---
+++ app/Http/Controllers/Angel/DossierController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\Angel;
+
+use App\Http\Controllers\Controller;
+use App\Models\Actif;
+use App\Models\Projet;
+use App\Models\TagsProjet;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class DossierController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+       return back();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Projet  $projet
+     * @return \Illuminate\Http\Response
+     */
+    public function show($p)
+    {
+        //
+
+	   // dd($p);
+	    $projet = Projet::all()->where('token',$p)->first();
+	    return view('/Angel/Dossiers/show')->with(compact('projet'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Projet  $projet
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Projet $projet)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Projet  $projet
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Projet $projet)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Projet  $projet
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Projet $projet)
+    {
+        //
+    }
+}
