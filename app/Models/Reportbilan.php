60cd0dece6bd3328ee7166a27447e4c2692e69d3
Clement <clementessomba@gmail.com>
2019-11-03 22:45:15 +0100

    petits changements

--- app/Models/Reportbilan.php
+++ app/Models/Reportbilan.php
@@ -9,7 +9,7 @@ class Reportbilan extends Model
     //
 	protected $guarded =[];
 	//public $timestamps=false;
-	protected $table = 'testresultats';
+	protected $table = 'testbilans';
 	public function projet(){
 		return $this->belongsTo('App\Models\Projet');
 	}
