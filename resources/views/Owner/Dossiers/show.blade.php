180e9feb121baaa398c94295b050a9ac9453c305
Clement <clementessomba@gmail.com>
2019-11-04 15:15:34 +0100

    petits changements

--- resources/views/Owner/Dossiers/show.blade.php
+++ resources/views/Owner/Dossiers/show.blade.php
@@ -1204,8 +1204,8 @@
     </div>
 </div>
 <script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
-<script src="{{ 'plugins/datatables/jquery.dataTables.js' }}"></script>
-<script src="{{'plugins/datatables-bs4/js/dataTables.bootstrap4.js'}}"></script>
+<script src="{{asset('plugins/datatables/jquery.dataTables.js') }}"></script>
+<script src="{{asset('plugins/datatables-bs4/js/dataTables.bootstrap4.js')}}"></script>
 <script>
   $(function () {
     $("#table-invest").DataTable();
