76304670da160236aad982c1d1c1071c770f2182
Clement <clementessomba@gmail.com>
2019-11-04 15:14:18 +0100

    petits changements

--- resources/views/Owner/Dossiers/show.blade.php
+++ resources/views/Owner/Dossiers/show.blade.php
@@ -1204,6 +1204,8 @@
     </div>
 </div>
 <script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
+<script src="{{ 'plugins/datatables/jquery.dataTables.js' }}"></script>
+<script src="{{'plugins/datatables-bs4/js/dataTables.bootstrap4.js'}}"></script>
 <script>
   $(function () {
     $("#table-invest").DataTable();
