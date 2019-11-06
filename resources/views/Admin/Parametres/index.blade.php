eab1faa1e654a7892a8db96011ef703dfc8c7f4b
Clement <clementessomba@gmail.com>
2019-11-06 09:35:34 +0100

    petits ajouts

--- resources/views/Admin/Parametres/index.blade.php
+++ resources/views/Admin/Parametres/index.blade.php
@@ -1,152 +1,40 @@
 @extends('......layouts.admin')
 
-@section('content-header')
-    <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Pays</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="/admin/dashboard">ACCUEIL</a></li>
-              <li class="breadcrumb-item">PARAMETRES</li>
-              <li class="breadcrumb-item active">PAYS</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-@endsection
-
 @section('content')
-
-    <div class="row">
-            <div class="col-12">
-              <div class="card">
-                <div class="card-header">
-                  <h3 class="card-title">LISTE DES PAYS</h3>
-                </div>
-                <!-- /.card-header -->
-                <div class="card-body">
-                  <table id="example1" class="table table-bordered table-hover table-condensed">
-                    <thead>
-                    <tr>
-                      <th>NOM</th>
-                      <th>COUNTRY CODE</th>
-                      <th>LATITUDE</th>
-                      <th>LONGITUDE</th>
-
-                      <th><a class="btn btn-primary btn-xs" href="#" data-toggle="modal" data-target="#modal-lg"><i class="fa fa-plus-circle"></i></a></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($pays as $ville)
-                          <tr>
-                              <td>{!! $ville->name !!} </td>
-                              <td>{!!$ville->code !!}</td>
-                              <td>{!! $ville->latitude !!} </td>
-                              <td>{!! $ville->longitude !!} </td>
-
-                              <td>
-                              <ul style="margin-bottom: 0" class="list-inline">
-                                <li class="list-inline-item"><a class="btn btn-default btn-xs" href="{{route('admin.pays.show',[$ville->id])}}"><i class="fa fa-search"></i></a></li>
-                              </ul>
-                              </td>
-                          </tr>
-                      @endforeach
-
-                    </tbody>
-                    <tfoot>
-                        <tr>
-                      <th>NOM</th>
-                      <th>COUNTRY CODE</th>
-                      <th>LATITUDE</th>
-                      <th>LONGITUDE</th>
-
-                      <th></th>
-                    </tr>
-                    </tfoot>
-                  </table>
-                </div>
-                <!-- /.card-body -->
-              </div>
-
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="card-title">PARAMETRES DE LA PLATEFORME</h5>
             </div>
+            <div class="card-body">
+                <form enctype="multipart/form-data" class="form" action="admin/params" method="post">
+                    {{csrf_field()}}
 
-            <!-- /.col -->
-          </div>
-
-           <div class="modal fade" id="modal-lg">
-                  <div class="modal-dialog modal-lg">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h4 class="modal-title">Nouveau pays</h4>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">&times;</span>
-                        </button>
-                      </div>
-                      <div class="modal-body">
-                        <form enctype="multipart/form-data" role="form" action="{{route('admin.pays.store')}}" method="post">
-                        {{csrf_field()}}
-                          <div class="card-body">
-                            <div class="form-group">
-                              <label for="name">NOM</label>
-                              <input type="text" class="form-control" id="name" name="name" placeholder="Saisir le nom de la ville">
-                            </div>
-
-                            <div class="form-group">
-                              <label for="code">CODE</label>
-                              <input type="text" class="form-control" id="code" name="code" placeholder="Saisir le country code du pay. Par FR pour la france ">
-                            </div>
-
-                            <div class="form-group">
-                              <label for="latitude">LATITUDE</label>
-                              <input type="text" class="form-control" id="latitude" name="latitude" placeholder="Saisir la latitude de la ville">
-                            </div>
-                            <div class="form-group">
-                              <label for="longitude">LONGITUDE</label>
-                              <input type="text" class="form-control" id="longitude" name="longitude" placeholder="Saisir la longitude de la ville">
-                            </div>
-
-
-
-                          </div>
-                          <!-- /.card-body -->
-
-                          <div class="card-footer">
-                            <button type="submit" class="btn btn-success"><i class="fa fa-w fa-save"></i> Enregistrer</button>
-                          </div>
-                        </form>
-                      </div>
-
+                    <div class="form-group">
+                        <label for="pacte">PACTE DES ACTIONNAIRES</label>
+                        <input type="file" class="form-control" name="pacte" id="pacte"/>
                     </div>
-                    <!-- /.modal-content -->
-                  </div>
-                  <!-- /.modal-dialog -->
-           </div>
+                    <div class="form-group">
+                        <label for="pret">CONTRAT DE PRET</label>
+                        <input type="file" class="form-control" name="pret" id="pret"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="actif">CONTRAT DE CESSION D'ACTIF</label>
+                        <input type="file" class="form-control" name="actif" id="actif"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="creance">CONTRAT DE CESSION DE CREANCE</label>
+                        <input type="file" class="form-control" name="creance" id="creance"/>
+                    </div>
+
+                     <button class="btn btn-block btn-success btn-sm"><i class="fa fa-save"></i> ENREGISTRER</button>
+
+                </form>
+            </div>
+        </div>
 
 
-<style>
-    .table th,
-    .table td {
-      padding: 0.35rem;
-      vertical-align: top;
-      border-top: 1px solid #dee2e6;
-    }
-  </style>
-
-  <link rel="stylesheet" href="{{asset('plugins/datatables-bs4/css/dataTables.bootstrap4.css')}}">
-
-
-<!-- DataTables -->
-<script src="{{asset('plugins/datatables/jquery.dataTables.js')}} "></script>
-<script src="{{asset('plugins/datatables-bs4/js/dataTables.bootstrap4.js')}}"></script>
-
-<script>
-  $(function () {
-    $("#example1").DataTable();
-
-  });
-</script>
+    </div>
 
 
 @endsection
