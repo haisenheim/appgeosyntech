bd42d6e3dbac258d0456b84fea29ad38b69b8a9e
Clement <clementessomba@gmail.com>
2019-11-10 16:16:52 +0100

    ajouts

--- resources/views/Owner/Dossiers/show.blade.php
+++ resources/views/Owner/Dossiers/show.blade.php
@@ -996,7 +996,7 @@
                                                                 @foreach($projet->prevresultats as $prevr)
                                                                     <th><?= $prevr->ebe ?></th>
                                                                     @if(!$loop->last)
-                                                                    <th>{{ $projet->variations['ebe'][++$i] }}</th>
+                                                                    <th>{{ $projet->variations['ebe'][$i++] }}</th>
                                                                     @endif
                                                                 @endforeach
                                                             </tr>
@@ -1016,7 +1016,7 @@
                                                                 @foreach($projet->prevresultats as $prevr)
                                                                     <th><?= $prevr->re ?></th>
                                                                     @if(!$loop->last)
-                                                                    <th>{{ $projet->variations['re'][++$i] }}</th>
+                                                                    <th>{{ $projet->variations['re'][$i++] }}</th>
                                                                     @endif
                                                                 @endforeach
                                                             </tr>
@@ -1044,7 +1044,7 @@
                                                                 @foreach($projet->prevresultats as $prevr)
                                                                     <th><?= $prevr->rf ?></th>
                                                                     @if(!$loop->last)
-                                                                    <th>{{ $projet->variations['rf'][++$i] }}</th>
+                                                                    <th>{{ $projet->variations['rf'][$i++] }}</th>
                                                                     @endif
                                                                 @endforeach
                                                             </tr>
@@ -1072,7 +1072,7 @@
                                                                 @foreach($projet->prevresultats as $prevr)
                                                                     <th><?= $prevr->re ?></th>
                                                                     @if(!$loop->last)
-                                                                    <th>{{ $projet->variations['rex'][++$i] }}</th>
+                                                                    <th>{{ $projet->variations['rex'][$i++] }}</th>
                                                                     @endif
                                                                 @endforeach
                                                             </tr>
