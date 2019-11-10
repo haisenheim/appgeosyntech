bcdb1377fe3933f726200226b8a20ba43cd9be35
Clement <clementessomba@gmail.com>
2019-11-10 15:11:07 +0100

    ajouts

--- app/Models/Prevresultat.php
+++ app/Models/Prevresultat.php
@@ -20,4 +20,26 @@ class Prevresultat extends Model
 	protected function getVaAttribute(){
 		return $this->getMbAttribute() + $this->pi + $this->ps + $this->sp + $this->ape - $this->taxes - $this->cf;
 	}
+	public  function getEbeAttribute(){
+		return $this->getVaAttribute()  - $this->salaires;
+	}
+
+	protected function getReAttribute(){
+		return $this->getEbeAttribute() +$this->rap +$this->tc - $this->dap;
+	}
+
+	protected function getRfAttribute(){
+		return $this->pf +$this->rapf +$this->tcf - $this->dapf - $this->cfi;
+	}
+	protected function getRexAttribute(){
+		return $this->pe +$this->pci +$this->rape + $this->tce - $this->vce -$this->dape - $this->ce;
+	}
+
+	protected function getRcaiAttribute(){
+		return $this->getReAttribute() + $this->getRfAttribute() + $this->getRexAttribute();
+	}
+
+	public function getRnAttribute(){
+		return $this->getRcaiAttribute() - $this->participations - $this->impots;
+	}
 }
