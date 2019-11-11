872879edac9445e947a77cb9a46fc56d3b8cfd97
Clement <clementessomba@gmail.com>
2019-11-11 19:54:48 +0100

    Ajouts

--- resources/views/Adminorg/Investissements/show.blade.php
+++ resources/views/Adminorg/Investissements/show.blade.php
@@ -1,6 +1,6 @@
 @extends('......layouts.adminorg')
 @section('content')
-<?= $client = $investissement->angel; dd($investissement); ?>
+<?php $client = $investissement->angel; dd($investissement->angel); ?>
 <div style="padding: 20px;" class="container-fluid">
         <div class="row">
             <div class="col-md-4 col-sm-12">
