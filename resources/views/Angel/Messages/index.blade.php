d927f0ff8d6ba62e250e5352450ada2ded90e81c
Clement <clementessomba@gmail.com>
2019-11-11 22:17:39 +0100

    Ajouts

--- resources/views/Angel/Messages/index.blade.php
+++ resources/views/Angel/Messages/index.blade.php
@@ -218,7 +218,7 @@
 
 
 <script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
-
+<script src="{{asset('js/tooltip.js')}}"></script>
   <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
 <script src="{{asset('dist/js/adminlte.min.js')}}"></script>
 <script src="{{asset('dist/js/demo.js')}}"></script>
