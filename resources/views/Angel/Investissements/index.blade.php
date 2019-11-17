72e1ced252b4d397a017f0cef029c7470a924135
Clement <clementessomba@gmail.com>
2019-11-17 16:16:20 +0100

    petit changement

--- resources/views/Angel/Investissements/index.blade.php
+++ resources/views/Angel/Investissements/index.blade.php
@@ -7,93 +7,106 @@
     <div style="padding-top: 30px" class="container-fluid">
 
 
-         <div class="row">
-          <div class="col-12">
-            <!-- Custom Tabs -->
-            <div class="card">
-              <div class="card-header d-flex p-0">
-                <h3 class="card-title p-3">MES INVESTISSEMENTS</h3>
-                <ul class="nav nav-pills ml-auto p-2">
-                  <li class="nav-item"><a class="nav-link active" href="#tab_1" data-toggle="tab">PROJETS DE LEVEE DE FONDS</a></li>
-
-                </ul>
-              </div><!-- /.card-header -->
-              <div class="card-body">
-                <div class="tab-content">
-                  <div class="tab-pane active" id="tab_1">
-                        <div class="row">
-                    @foreach($investissements as $investissement)
-                        <?php $projet = $investissement->projet ?>
-                         <div class="col-md-3">
-                            <!-- Widget: user widget style 1 -->
-                             <div class="card card-widget widget-user position-relative">
-                              <!-- Add the bg color to the header using any of the bg-* classes -->
-                              <div class="widget-user-header text-white"
-                                   style="background: url('{{ $projet->imageUri?asset('img/'.$projet->imageUri):asset('img/logo.png') }}'); background-size: cover">
-                                    <div class="ribbon-wrapper ribbon-xl">
-                                        <div class="ribbon bg-primary">
-                                            {{$projet->montant}} FCFA
-                                        </div>
-                                    </div>
-                                    <h3 style="font-weight: 900" class="widget-user-username text-left"><?= $projet->name ?></h3>
-                                    <h5 style="font-weight: 700" class="widget-user-desc text-right">{{ $projet->owner->name }}</h5>
-                              </div>
-                              <div class="widget-user-image">
-                                <img class="img-circle" src="{{$projet->user?$projet->user->imageUri? asset('img/'.$projet->user->imageUri):asset('img/avatar.png'):asset('img/avatar.png')}}" alt="User Avatar">
-                              </div>
-                              <div class="card-body">
-
-                              </div>
-                              <div style="padding: .75rem 1rem" class="card-footer ">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                          <span class="description-text"><i class="fa fa-map-marker"></i> {{ $projet->ville->name  }}</span>
-                                    </div>
-                                    <div class="col-sm-12 col-md-5">
-                                        <div class="btn-app">
-
-                                      <span class="badge bg-info">{{ $projet->total }}</span>
-                                      <i class="fas fa-users"></i> Users
-
-                                        </div>
-
-                                    </div>
-                                    <div class="col-sm-12 col-md-7">
-                                        <div class="progress">
-                                              <div class="progress-bar bg-primary progress-bar-striped" role="progressbar" aria-valuenow="{{ $projet->pourcentage }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $projet->pourcentage }}%">
-                                                <span class="sr-only">{{ $projet->pourcentage }}% recouvert</span>
-                                              </div>
-                                        </div>
-                                    </div>
-                                  <!-- /.col -->
-                                </div>
-                                <?php if($investissement->validated): ?>
-                                <a href="/angel/dossiers/{{ $investissement->token }}" class="btn btn-block btn-outline-success btn-xs btn-p">DATA ROOM</a>
-                                <!-- /.row -->
-                                <?php endif ?>
-                              </div>
-                            </div>
-                            <!-- /.widget-user -->
-                        </div>
-                    @endforeach
-                </div>
-                <div class="">
-                        <ul class="pagination justify-content-end">
-                        {{ $investissements->links() }}
-                    </ul>
-                  </div>
-                  </div>
-                  <!-- /.tab-pane -->
 
 
-                </div>
-                <!-- /.tab-content -->
-              </div><!-- /.card-body -->
-            </div>
-            <!-- ./card -->
+
+        <div class="card">
+        <div class="card-header">
+
+          <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+              <i class="fas fa-minus"></i></button>
+            <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
+              <i class="fas fa-times"></i></button>
           </div>
-          <!-- /.col -->
         </div>
+
+        <div class="card-body p-0">
+          <table class="table table-striped projects" id="table-projets">
+              <thead>
+                  <tr>
+                      <th style="width: 1%">
+                          #
+                      </th>
+                      <th style="width: 38%">
+                          Projet
+                      </th>
+                      <th style="width: 20%">
+                          Promoteur
+                      </th>
+                      <th>Progression</th>
+
+                      <th style="width: 20%">
+                      </th>
+                  </tr>
+              </thead>
+              <tbody>
+
+
+                   @foreach($investissements as $investissement)
+                        <?php $projet= $investissement->projet ?>
+                        <tr>
+                            <td>#</td>
+                            <td>
+                            <span class="text-bold text-lg-left">{{ $projet->name }} - <b>{{ $projet->montant }} {{ $projet->devise->name }}</b></span>- <small>{{ $investissement->created_at?date_format($investissement->created_at,'d/m/Y'):'' }}</small>  - <span class="badge badge-default"><i class="fa fa-map-marker"></i>&nbsp; {{ $projet->ville->name  }}</span> <br/>
+
+                            </td>
+
+                            <td>{{$projet->owner?$projet->owner->name:'-'}}</td>
+                            <td>
+                                <div class="progress">
+                                    <div class="progress-bar bg-primary progress-bar-striped" role="progressbar" aria-valuenow="{{ $projet->pourcentage }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $projet->pourcentage }}%">
+                                      <span class="sr-only">{{ $projet->pourcentage }}% recouvert</span>
+                                    </div>
+                                </div>
+                            </td>
+
+
+                              <td class="project-actions text-right">
+                                    @if($investissement->validated)
+                                          <a class="btn btn-primary btn-xs" href="/angel/dossiers/{{ $investissement->token  }}">
+                                              <i class="fas fa-folder">
+                                              </i>
+                                              Data room
+                                          </a>
+                                  @endif
+
+
+                              </td>
+                        </tr>
+                   @endforeach
+              </tbody>
+          </table>
+        </div>
+        <!-- /.card-body -->
+      </div>
+
+        <link rel="stylesheet" href="{{asset('plugins/datatables-bs4/css/dataTables.bootstrap4.css')}}">
+        <!-- jQuery -->
+        <script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
+
+
+
+
+        <!-- AdminLTE for demo purposes -->
+        <script src="{{asset('dist/js/demo.js')}}"></script>
+        <!-- DataTables -->
+        <script src="{{asset('plugins/datatables/jquery.dataTables.js')}} "></script>
+        <script src="{{asset('plugins/datatables-bs4/js/dataTables.bootstrap4.js')}}"></script>
+
+        <script>
+          $(function () {
+
+            $('#table-projets').DataTable({
+              "paging": true,
+              "lengthChange": false,
+
+              "ordering": true,
+              "info": true,
+              "autoWidth": false
+            });
+          });
+        </script>
     </div>
 
 
