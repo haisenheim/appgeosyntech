a438221674de108f99ccf527325f44cadd0f0d47
Clement <clementessomba@gmail.com>
2019-11-18 13:11:10 +0100

    petit ajout

--- resources/views/Angel/Actifs/index.blade.php
+++ resources/views/Angel/Actifs/index.blade.php
@@ -16,10 +16,11 @@
 
                     <!-- small card -->
                     <div class="small-box bg-success">
-                      <div style="background: url('{{ $projet->imageUri?asset('img/'.$projet->imageUri):asset('img/logo.png') }}')  cover;" class="inner">
+                      <div class="inner">
                         <h3>{{ $projet->prix }}<sup style="font-size: 20px">{{ $projet->devise->abb }}</sup></h3>
 
                         <p>{{ $projet->name }}</p>
+                        <img src="{{ $projet->imageUri?asset('img/'.$projet->imageUri):asset('img/logo.png') }}" style="width: 100px; height: 100px; border-radius: 50px; float: right"/>
                       </div>
 
                       <a data-target="#viewModal" data-toggle="modal" href="#" class="small-box-footer">
@@ -28,46 +29,7 @@
                     </div>
 
 
-            <!-- Widget: user widget style 1 -->
-                        <a style="color:#555" href="/angel/actifs/{{ $projet->token  }}">
-                            <div class="card card-widget widget-user">
-                              <!-- Add the bg color to the header using any of the bg-* classes -->
-                              <div class="widget-user-header text-white"
-                                   style="background: url('{{ $projet->imageUri?asset('img/'.$projet->imageUri):asset('img/logo.png') }}') center center;">
-                                   <div style="background: #EEEEEE;">
-                                   <h3 style="font-weight:900; color: #4caf50;" class="widget-user-username text-right"><?= $projet->name ?></h3>
-                                    <h5 style="font-weight: 700;color: #4caf50;" class="widget-user-desc text-right">{{ $projet->owner->name }}</h5>
-                                   </div>
 
-                              </div>
-                              <div class="widget-user-image">
-                                <img class="img-circle" src="{{$projet->user?$projet->user->imageUri? asset('img/'.$projet->user->imageUri):asset('img/avatar.png'):asset('img/avatar.png')}}" alt="User Avatar">
-                              </div>
-                              <div style="padding: .75rem 1.25rem;" class="card-footer">
-                                <div class="row">
-                                    <div class="col-md-6 border-right">
-                                        <div class="description-block">
-                                          <h5 class="description-header"><i class="fa fa-map-marker"></i></h5>
-                                          <span class="description-text">{{ $projet->ville->name  }}</span>
-                                        </div>
-                                    </div>
-
-                                  <!-- /.col -->
-                                  <div class="col-sm-6">
-                                    <div class="description-block">
-                                      <h5 class="description-header"><i class="fa fa-coins"></i></h5>
-                                      <span class="description-text">{{$projet->prix}} FCFA</span>
-                                    </div>
-                                    <!-- /.description-block -->
-                                  </div>
-                                  <!-- /.col -->
-                                </div>
-                                <!-- /.row -->
-                              </div>
-                            </div>
-                        </a>
-
-            <!-- /.widget-user -->
                     </div>
 
                     @endforeach
