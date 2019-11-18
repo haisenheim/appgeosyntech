602f87e3be32d45adce6a7a2309bbbb797c4202c
Clement <clementessomba@gmail.com>
2019-11-18 10:53:42 +0100

    petit ajout

--- resources/views/Owner/Actifs/show.blade.php
+++ resources/views/Owner/Actifs/show.blade.php
@@ -221,7 +221,7 @@
       <div class="modal-dialog modal-lg">
           <div class="modal-content">
               <div class="modal-header bg-success">
-          <h4 class="modal-title">LISTE DE POTENTIELS INVESTISSEMENT</h4>
+          <h4 class="modal-title">LISTE DE POTENTIELS INVESTISSEURS</h4>
           <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">x</span></button>
       </div>
       <div class="modal-body">
@@ -280,6 +280,39 @@
 </div
 
 
+<div class="modal fade" id="upDocsModal">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header bg-success">
+                <h6  class="modal-title text-center">CHARGEMENT DES DOCUMENTS</h6>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+            <div class="modal-body">
+            <div class="card card-danger">
+                <div class="card-body">
+                    <form action="/owner/actif/docs/" method="post" enctype="multipart/form-data">
+                        {{csrf_field()}}
+                        <input type="hidden" name="projet_token" value="{{ $projet->token }}"/>
+                        <div class="form-group">
+                            <label for="ordre">ORDRE DE VIREMENT</label>
+                            <input type="file" class="form-control" id="ordre" name="ordre"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="actif">CONTRAT DE CESSION D'ACTIF</label>
+                            <input type="file" class="form-control" id="actif" name="actif"/>
+                        </div>
+
+                        <button type="submit" class="btn btn-outline-success btn-block"> <i class="fa fa-save fa-lg"></i> ENREGISTRER</button>
+                    </form>
+
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 <style>
