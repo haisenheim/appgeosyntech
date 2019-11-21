ea7c30510c529ab2a8b7bdda4e7472d0cf564f0f
Clement <clementessomba@gmail.com>
2019-11-21 11:52:19 +0100

    Ajouts

--- resources/views/Consultant/Dossiers/show.blade.php
+++ resources/views/Consultant/Dossiers/show.blade.php
@@ -819,31 +819,6 @@
                   </div>
               </div>
 
-    <script type="text/javascript" src="{{ asset('js/api.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('summernote/dist/summernote.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('summernote/lang/summernote-fr-FR.js') }}"></script>
-    <link rel="stylesheet" href="{{ asset('summernote/dist/summernote.css') }}"/>
-
-    <script type="text/javascript">
-        $(document).ready(function() {
-          $('#synthese1').summernote({
-            height: 300,
-            tabsize: 2,
-            followingToolbar: true,
-            lang:'fr-FR'
-          });
-        });
-    </script>
-    <script type="text/javascript">
-       $(document).ready(function() {
-         $('#synthese2').summernote({
-           height: 300,
-           tabsize: 2,
-           followingToolbar: true,
-           lang:'fr-FR'
-         });
-       });
-     </script>
 
     <script>
         $(document).ready(function(){
@@ -1124,22 +1099,6 @@
         			</div>
         		</div>
         	</form>
-
-                <script type="text/javascript">
-                    $(document).ready(function() {
-                      $('textarea').summernote({
-                        height: 300,
-                        tabsize: 2,
-                        followingToolbar: true,
-                        lang:'fr-FR'
-                      });
-
-                    });
-
-
-                  </script>
-
-
         </div>
 
 <style>
@@ -1298,9 +1257,13 @@
                overflow-y: scroll;
            }
 </style>
-<script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
+
 <script src="{{asset('plugins/datatables/jquery.dataTables.js') }}"></script>
 <script src="{{asset('plugins/datatables-bs4/js/dataTables.bootstrap4.js')}}"></script>
+<script type="text/javascript" src="{{ asset('js/api.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('summernote/dist/summernote.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('summernote/lang/summernote-fr-FR.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('summernote/dist/summernote.css') }}"/>
 <script>
   $(function () {
     $("#table-invest").DataTable();
@@ -1308,6 +1271,136 @@
   });
 </script>
 
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('textarea').summernote({
+      height: 300,
+      tabsize: 2,
+      followingToolbar: true,
+      lang:'fr-FR'
+    });
+
+  });
+</script>
+
+<script>
+        $(document).ready(function(){
+           // $('#side2').height($('#side1').height());
+           //$('#side2').height(890);
+            getPlan($('#plan_id').val());
+
+            $.ajax({
+                url: "/consultant/dossier/getchoices",
+                type:'Get',
+                dataType:'json',
+                data:{id:$('#id').val()},
+                success:function(data){
+                    if(data!=null){
+                        $.ajax({
+                            url:orm+'carto',
+                            type:'Post',
+                            dataType:'json',
+                            data:{choix:data},
+                            success:function(rep){
+                                $('#risks-loader').hide();
+
+                                var html = '';
+                                //console.log(Object.entries(rep));
+                                var risks=Object.entries(rep);
+                                for(var i=0; i<risks.length;i++){
+
+                                    var rs= parseInt(risks[i][1].length) + 1;
+                                    var tr= '<tr><th style="align-content: center; margin-top: auto" align="center" rowspan='+ rs  +'>'+ risks[i][0] +'</th></tr>';
+                                    html=html+tr;
+                                    for(var k=0; k<risks[i][1].length; k++){
+                                        $value = risks[i][1][k];
+                                        $cb= parseInt($value.question.produits_risque.frequence) * parseInt($value.question.produits_risque.gravite);
+                                        $cn=parseInt($value.question.produits_risque.frequence) * parseInt($value.question.produits_risque.gravite) * parseFloat($value.taux);
+
+                                        if(parseFloat($cb) >= 13){
+                                            $clrb='red';
+                                        }else{
+                                            if( parseFloat($cb) >=4 && parseFloat($cb) <= 12){
+                                                $clrb='yellow';
+                                            }else{
+                                                $clrb = '#0ac60a';
+                                            }
+                                        }
+
+                                        if( parseFloat($cn) >= 13){
+                                            $clr='red';
+                                        }else{
+                                            if( parseFloat($cn) >=4 &&  parseFloat($cn) <= 12){
+                                                $clr='yellow';
+                                            }else{
+                                                $clr = '#0ac60a';
+                                            }
+                                        }
+
+                                        var trr = '<tr>'+
+                                            '<td>'+ $value.question.produits_risque.name +'</td>'+
+                                            '<td>'+$value.question.produits_risque.causes +'</td>'+
+                                            '<td>'+ $value.question.produits_risque.consequences +'</td>'+
+                                            '<td>'+ $value.question.produits_risque.frequence +'</td>'+
+                                            '<td>'+ $value.question.produits_risque.gravite+'</td>'+
+                                            '<td style="background-color:'+ $clrb +'; font-weight: 900; text-align: right">'+ $cb  +'</td>'+
+                                            '<td style="background-color:'+ $clr +'">'+ $cn +'</td>'+
+                                        '</tr>';
+
+                                        html=html+trr;
+
+                                        //console.log(risks[i][1][k]);
+                                    }
+                                   // console.log(risks[i][1]);
+                                }
+
+                                $('#risques-tab').find('tbody').html(html);
+                            },
+                            Error:function(){
+                                $('#risks-loader').hide();
+                            }
+                        });
+                    }
+
+                }
+            })
+        });
+
+        function getPlan(id){
+
+                 $.ajax({
+                   url:orm+'get-plan',
+                   type:'Get',
+                   dataType:'json',
+                   data:{id:id},
+                       success:function(data){
+                           //console.log(data);
+                           if(data!=null){
+                                $.ajax({
+                                  url:orm+'get-plan',
+                                  type:'Get',
+                                  dataType:'json',
+                                  data:{id:id},
+                                success:function(){
+                                }
+                                });
+                           }
+                           var html = '';
+                           var pls = data.plignes;
+                           for(var i = 0; i<data.plignes.length; i++){
+
+                                var tr ='<tr data-id="'+ pls[i].id +'"><td style="width: 13%">'+ pls[i].produits_risque.risque.name +'</td><td style="width: 20%">'+ pls[i].produits_risque.produit.name +'</td><td style="width: 20%">'+ pls[i].produits_risque.name +'</td><td contenteditable="true" style="width: 37%">'+ pls[i].amelioration +'</td></tr>';
+                                html = html + tr;
+                           }
+                           $('#example').find('tbody').html(html);
+                       },
+                   Error:function(){
+
+                   }
+                 });
+            }
+
+    </script>
 @endsection
 
 
