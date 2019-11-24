f0cb9a07c1890f0f1d2e538d64a90e0c719640b2
Clement <clementessomba@gmail.com>
2019-11-24 20:44:09 +0100

    ajouts

--- routes/web.php
+++ routes/web.php
@@ -192,7 +192,7 @@ Route::prefix('consultant')
 	    Route::get('projet/create-diag-externe/{token}','EarlyController@createDiagExterne');
 	    Route::post('projet/save-diag-externe','EarlyController@saveDiagExterne');
 	    Route::get('projet/update-plan','EarlyController@updatePlanJson');
-	    Route::get('projt/add-mode','EarlyController@addMode');
+	    Route::get('projet/add-mode','EarlyController@addMode');
 
 	    Route::resource('partenariats','InfrastructureController');
 	    Route::post('partenariat/synthese1','InfrastructureController@synthese1');
