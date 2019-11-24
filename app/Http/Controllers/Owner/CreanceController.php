c645db36499315ff36641fc2d567acf40ee2567e
Clement <clementessomba@gmail.com>
2019-11-24 11:26:37 +0100

    Ajouts

--- app/Http/Controllers/Owner/CreanceController.php
+++ app/Http/Controllers/Owner/CreanceController.php
@@ -214,8 +214,8 @@ class CreanceController extends Controller
     {
         //
 	    //$creance= Creance::where('token',$request->token)->first();
-		//$creance=Creance::updateOrCreate(['token',$request->token],['debiteur'=>$request->debiteur,'address'=>$request->address,'phone'=>$request->phone,'montant'=>$request->montant,'prix_cession'=>$request->prix_cession,'dt_paiement'=>$request->dt_paiement,'devise_id'=>$request->devise_id]);
-	   $creance=Creance::updateOrCreate(['token',$request->token],$request->input());
+		$creance=Creance::updateOrCreate(['token',$request->token],['debiteur'=>$request->debiteur,'address'=>$request->address,'phone'=>$request->phone,'montant'=>$request->montant,'prix_cession'=>$request->prix_cession,'dt_paiement'=>$request->dt_paiement,'devise_id'=>$request->devise_id]);
+	  // $creance=Creance::updateOrCreate(['token',$request->token],$request->input());
 	    return '/owner/creances/'.$creance['token'];
     }
 
