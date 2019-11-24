d7cd087801cd8bbf7df1ecadafcc9e323b4ef09d
Clement <clementessomba@gmail.com>
2019-11-24 22:28:19 +0100

    ajouts

--- app/Models/EarliesMoyen.php
+++ app/Models/EarliesMoyen.php
@@ -10,4 +10,11 @@ class EarliesMoyen extends Model
 	protected $guarded = [];
 	public $timestamps = false;
 	protected $table ='earlies_moyens';
+	public function moyen(){
+		return $this->belongsTo('App\Models\Moyen');
+	}
+
+	public function earlie(){
+		return $this->belongsTo('App\Models\Earlie');
+	}
 }
