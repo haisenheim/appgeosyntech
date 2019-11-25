1f6131a0137c11ff09009e3d3d1506e809821b3c
Clement <clementessomba@gmail.com>
2019-11-25 21:59:22 +0100

    ajouts

--- resources/views/Admin/Infrastructures/show.blade.php
+++ resources/views/Admin/Infrastructures/show.blade.php
@@ -1832,7 +1832,100 @@
                   @endif
                   </div>
 
-              </div>
+    </div>
+
+    <div class="modal fade" id="uploadAppelModal">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header bg-success">
+                    <h6  class="modal-title text-center">PUBLICATION DE L'APPEL D'OFFRE</h6>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                <div class="modal-body">
+                <div class="card card-danger">
+                    <div class="card-body">
+                        <form action="/admin/partenariat/publish" method="post" enctype="multipart/form-data">
+                            <div class="form-group">
+                                <label for="appelUri">APPEL D'OFFRE</label>
+                                <input type="file" class="form-control" name="appelUri"/>
+                            </div>
+                            <button class="btn btn-block btn-outline-success btn-sm"><i class="fa fa-upload"></i> PUBLIER</button>
+                        </form>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="angelsModal">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header bg-success">
+                    <h6  class="modal-title text-center">CONCESSIONNAIRES POTENTIELS</h6>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                <div class="modal-body">
+                <div class="card card-danger">
+                    <div class="card-body">
+                         @if($projet->concessions->count() >=1)
+                            <table style="color: #000" id="table-invest" class="table table-bordered table-hover">
+                                <thead>
+                                <tr>
+                                  <th>#</th>
+                                  <th>Depuis le</th>
+                                  <th>RDV</th>
+                                  <th>STATUT</th>
+                                  <th></th>
+
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($projet->concessions as $invest)
+                                        <tr>
+                                            <td>{{ $invest->angel->name }}</td>
+                                            <td><?= $invest->created_at?date_format($invest->created_at, 'd/m/Y H:i'):'-' ?></td>
+                                            <td><?= $invest->rencontre ?></td>
+                                            <td></td>
+                                            <td>
+
+                                                <div class="btn-group">
+                                                    <button type="button" class="btn btn-default btn-flat">Actions</button>
+                                                    <button type="button" class="btn btn-default btn-flat dropdown-toggle" data-toggle="dropdown">
+                                                      <span class="caret"></span>
+
+                                                    </button>
+                                                    <div class="dropdown-menu" role="menu">
+                                                     <?php if($invest->lettre): ?>
+                                                        <a class="dropdown-item" href="#">Lettre d'intention</a>
+                                                      <?php endif; ?>
+                                                      <?php if($invest->validated): ?>
+                                                        <a class="dropdown-item" href="/owner/investissements/close/{{ $invest->token }}">Fermer la data room</a>
+                                                      <?php else: ?>
+                                                        <a class="dropdown-item" href="/owner/investissements/open/{{ $invest->token }}">Ouvrir la data room</a>
+                                                      <?php endif; ?>
+
+                                                    </div>
+                                                  </div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+
+                    @endif
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 
     <script type="text/javascript" src="{{ asset('js/api.js') }}"></script>
 
@@ -2102,9 +2195,7 @@
 
 @endsection
 
-@section('action')
 
-@endsection
 
 @section('nav_actions')
 <main>
@@ -2175,8 +2266,6 @@
                    </li>
             @endif
 
-
-
         </ul>
         <div
          style="
@@ -2190,97 +2279,6 @@
         class="menu-bg"></div>
     </nav>
 </main>
-
-<div class="modal fade" id="uploadAppelModal">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header bg-success">
-                <h6  class="modal-title text-center">PUBLICATION DE L'APPEL D'OFFRE</h6>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-            <div class="modal-body">
-            <div class="card card-danger">
-                <div class="card-body">
-                    <form action="/admin/partenariat/publish" method="post" enctype="multipart/form-data">
-                        <div class="form-group">
-                            <label for="appelUri">APPEL D'OFFRE</label>
-                            <input type="file" class="form-control" name="appelUri"/>
-                        </div>
-                        <button class="btn btn-block btn-outline-success btn-sm"><i class="fa fa-upload"></i> PUBLIER</button>
-                    </form>
-                </div>
-            </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="modal fade" id="angelsModal">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header bg-success">
-                <h6  class="modal-title text-center">CONCESSIONNAIRES POTENTIELS</h6>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-            <div class="modal-body">
-            <div class="card card-danger">
-                <div class="card-body">
-                     @if($projet->concessions->count() >=1)
-                        <table style="color: #000" id="table-invest" class="table table-bordered table-hover">
-                            <thead>
-                            <tr>
-                              <th>#</th>
-                              <th>Depuis le</th>
-                              <th>RDV</th>
-                              <th>STATUT</th>
-                              <th></th>
-
-                            </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($projet->concessions as $invest)
-                                    <tr>
-                                        <td>{{ $invest->angel->name }}</td>
-                                        <td><?= $invest->created_at?date_format($invest->created_at, 'd/m/Y H:i'):'-' ?></td>
-                                        <td><?= $invest->rencontre ?></td>
-                                        <td></td>
-                                        <td>
-
-                                            <div class="btn-group">
-                                                <button type="button" class="btn btn-default btn-flat">Actions</button>
-                                                <button type="button" class="btn btn-default btn-flat dropdown-toggle" data-toggle="dropdown">
-                                                  <span class="caret"></span>
-
-                                                </button>
-                                                <div class="dropdown-menu" role="menu">
-                                                 <?php if($invest->lettre): ?>
-                                                    <a class="dropdown-item" href="#">Lettre d'intention</a>
-                                                  <?php endif; ?>
-                                                  <?php if($invest->validated): ?>
-                                                    <a class="dropdown-item" href="/owner/investissements/close/{{ $invest->token }}">Fermer la data room</a>
-                                                  <?php else: ?>
-                                                    <a class="dropdown-item" href="/owner/investissements/open/{{ $invest->token }}">Ouvrir la data room</a>
-                                                  <?php endif; ?>
-
-                                                </div>
-                                              </div>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-
-                @endif
-                </div>
-            </div>
-            </div>
-        </div>
-    </div>
-</div>
 
 
 
