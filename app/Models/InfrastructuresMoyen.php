fb896cea84865830a5dd02e6dc4f62bb5ffac2d7
Clement <clementessomba@gmail.com>
2019-11-27 11:41:51 +0100

    ajout

--- app/Models/InfrastructuresMoyen.php
+++ app/Models/InfrastructuresMoyen.php
@@ -10,4 +10,12 @@ class InfrastructuresMoyen extends Model
 	protected $guarded = [];
 	public $timestamps = false;
 	protected $table ='infrastructures_moyens';
+
+	public function moyen(){
+		return $this->belongsTo('App\Models\Moyen');
+	}
+
+	public function infrastructure(){
+		return $this->belongsTo('App\Models\Infrastructure');
+	}
 }
