2afd866d6a181d697b512aceecc9c0ecef3abe01
Clement <clementessomba@gmail.com>
2019-11-28 18:15:07 +0100

    ajout

--- app/Http/Controllers/Angel/InvestissementDossierController.php
+++ app/Http/Controllers/Angel/InvestissementDossierController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Angel;
 use App\Http\Controllers\Controller;
 use App\Models\Flettre;
 use App\Models\Investissement;
+use App\Models\Lettre;
 use App\Models\Projet;
 use App\Models\Tletrre;
 use Illuminate\Http\Request;
@@ -23,6 +24,46 @@ class InvestissementDossierController extends Controller
 	    $investissements = Investissement::orderBy('created_at','desc')->where('projet_id','>',0)->where('angel_id',Auth::user()->id)->paginate(8);
 	    return view('Angel/Investissements/Projets/index')->with(compact('investissements'))->with('Liste des investissements');
     }
+
+	public function saveLetter(Request $request){
+		$projet = Investissement::where('token', $request->token)->first();
+		if($projet->lettre){
+			$lettre = $projet->lettre;
+			$data = [];
+			//$data['id']=$lettre->id;
+			$data['type_remboursement']= $request->type_remboursement;
+			$data['forme_id'] = $request->forme_id;
+			$data['montant'] = $request->montant;
+			$data['pct_participation']=$request->pct_participation;
+			$data['pct_engagement']=$request->pct_engagement;
+			$data['duree_engagement']=$request->duree_engagement;
+			$data['mt_engagement']=$request->mt_engagement;
+
+			$data['personnel']=$request->personnel;
+			$data['pct_pret']=$request->pct_pret;
+			$data['duree_pret']=$request->duree_pret;
+			$data['lieu'] = $request->lieu;
+			Lettre::updateOrCreate(['investissement_id'=>$lettre->investissement_id],$data);
+		}else{
+			$lettre =new Lettre();
+			$lettre->investissement_id = $projet->id;
+			$lettre->type_remboursement= $request->type_remboursement;
+			$lettre->forme_id = $request->forme_id;
+			$lettre->montant = $request->montant;
+			$lettre->pct_participation=$request->pct_participation;
+			$lettre->pct_engagement=$request->pct_engagement;
+			$lettre->duree_engagement=$request->duree_engagement;
+			$lettre->mt_engagement=$request->mt_engagement;
+
+			$lettre->personnel=$request->personnel;
+			$lettre->pct_pret=$request->pct_pret;
+			$lettre->duree_pret=$request->duree_pret;
+			$lettre->lieu = $request->lieu;
+			$lettre->save();
+		}
+
+		return back();
+	}
 
     /**
      * Show the form for creating a new resource.
