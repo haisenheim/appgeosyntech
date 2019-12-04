7cadbbba82c8f597707f8a73fb3e22cffa8fa493
Clement <clementessomba@gmail.com>
2019-12-04 10:26:21 +0100

    ajout

--- resources/views/Front/index.blade.php
+++ resources/views/Front/index.blade.php
@@ -412,7 +412,7 @@
 		</div>
 		<div class="nav navbar navbar-dark navbar-expand-md">
             <div class="container">
-            				<a class="navbar-brand" style="background: none; text-indent: 0" href="">OBAC ALERT</a>
+            				<a class="navbar-brand" style="background: none; text-indent: 0" href=""><i><img src="img/bg-obac.png" alt=""/></i> OBAC ALERT</a>
             			    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             			        <span class="navbar-toggler-icon"></span>
             			    </button>
