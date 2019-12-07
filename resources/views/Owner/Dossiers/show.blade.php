140d670a8874bf80afe65e19875c11460adc3366
Clement <clementessomba@gmail.com>
2019-12-07 12:52:17 +0100

    ajouts

--- resources/views/Owner/Dossiers/show.blade.php
+++ resources/views/Owner/Dossiers/show.blade.php
@@ -2140,7 +2140,7 @@
 
                     var tsr = $('#has_teaser').val();
                     if(tsr){
-                        setTimeout(function(){$('#popup').show()}, 7000);
+                        setTimeout($('#popup').show(), 7000);
                     }
 
                     });
@@ -2170,48 +2170,62 @@
     </style>
 
    @if($projet->teaser)
-        		    <input type="hidden" id="has_teaser" value="1"/>
-        		    <div class="modal" id="popup" tabindex="-1" role="dialog" aria-labelledby="addModalLabel">
-                        <div class="modal-dialog modal-lg" role="document">
-                            <div class="modal-content">
-                                 <div class="modal-header">
+           <input type="hidden" id="has_teaser" value="1"/>
+           <div class="modal" id="popup" tabindex="-1" role="dialog" aria-labelledby="addModalLabel">
+               <div class="modal-dialog modal-lg" role="document">
+                   <div class="modal-content">
+                        <div class="modal-header">
 
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                      <span aria-hidden="true">&times;</span>
-                                    </button>
-                                  </div>
-                                <div class="modal-body">
-                                    <div class="row">
-                                        <div class="col-md-5 col-sm-12">
-                                             <div style="background: url('{{asset('img/'.$projet->imageUri)}}'); background-size: cover; height: 300px; width: 100%" id="popup-img">
+                           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                             <span aria-hidden="true">&times;</span>
+                           </button>
+                         </div>
+                       <div class="modal-body">
+                           <div class="row">
+                               <div class="col-md-5 col-sm-12">
+                                    <div style="background: url('{{asset('img/'.$projet->imageUri)}}'); background-size: cover; height: 300px; width: 100%" id="popup-img">
 
-                                             </div>
-                                        </div>
-                                        <div class="col-md-7 col-sm-12">
-                                                <p>Félicitations, votre teaser a été enregistré et Il a été envoyé à notre réseau d’investisseurs. </p>
-
-                                                   <p> Vous pouvez dès lors :</p>
-                                                   <ul style="list-style: decimal">
-                                                        <li>Discuter avec les investisseurs en allant sur l’onglet « messagerie »</li>
-                                                        <li>Les rencontrer en étant accompagné d’un consultant du cabinet OBAC</li>
-                                                        <li>Leur donner accès à la « Data Room » en allant sur l’onglet « Liste des investisseurs » puis sur « Actions » et enfin  en cliquant sur « ouvrir la data room »</li>
-                                                        <li>Consulter les lettres d’intention dès lors qu’elles seront envoyées en cliquant sur « Liste des investisseurs » puis sur « Actions » et enfin sur « Lettre d’intention »</li>
-                                                        <li>Proposer aux investisseurs un contrat en se basant sur la documentation juridique présente dans l’onglet « MODELE DE DOCUMENT »</li>
-                                                        <li>Procéder à la signature de la documentation juridique et à la mise en ligne du document juridique. Après validation de ce document juridique par OBAC, l’investisseur procédera au versement des fonds sur un numéro de compte qui lui sera indiqué. </li>
-                                                        <li>Recevoir les fonds levés, déduits de la commission de succès de 5% du cabinet OBAC, des frais annuels d’abonnement à OBAC RISK MANAGEMENT pour la maitrise des risques de votre projet et d’une commission de gestion de x% sur le montant non décaissé. Le décaissement se fera par tranche en tenant compte de chaque étape de votre projet. </li>
-                                                        <li>Rédiger des rapports de gestion mensuels à destination des investisseurs</li>
-                                                   </ul>
-
-                                        </div>
                                     </div>
-                                </div>
+                               </div>
+                               <div class="col-md-7 col-sm-12">
+                                       <p>Félicitations, votre teaser a été enregistré et Il a été envoyé à notre réseau d’investisseurs. </p>
 
-                            </div>
-                        </div>
-                    </div>
-        		@else
-        		    <input type="hidden" id="has_teaser" value="0"/>
-        		@endif
+                                          <p> Vous pouvez dès lors :</p>
+                                          <ul style="list-style: decimal">
+                                               <li>Discuter avec les investisseurs en allant sur l’onglet « messagerie »</li>
+                                               <li>Les rencontrer en étant accompagné d’un consultant du cabinet OBAC</li>
+                                               <li>Leur donner accès à la « Data Room » en allant sur l’onglet « Liste des investisseurs » puis sur « Actions » et enfin  en cliquant sur « ouvrir la data room »</li>
+                                               <li>Consulter les lettres d’intention dès lors qu’elles seront envoyées en cliquant sur « Liste des investisseurs » puis sur « Actions » et enfin sur « Lettre d’intention »</li>
+                                               <li>Proposer aux investisseurs un contrat en se basant sur la documentation juridique présente dans l’onglet « MODELE DE DOCUMENT »</li>
+                                               <li>Procéder à la signature de la documentation juridique et à la mise en ligne du document juridique. Après validation de ce document juridique par OBAC, l’investisseur procédera au versement des fonds sur un numéro de compte qui lui sera indiqué. </li>
+                                               <li>Recevoir les fonds levés, déduits de la commission de succès de 5% du cabinet OBAC, des frais annuels d’abonnement à OBAC RISK MANAGEMENT pour la maitrise des risques de votre projet et d’une commission de gestion de x% sur le montant non décaissé. Le décaissement se fera par tranche en tenant compte de chaque étape de votre projet. </li>
+                                               <li>Rédiger des rapports de gestion mensuels à destination des investisseurs</li>
+                                          </ul>
+
+                               </div>
+                           </div>
+                       </div>
+
+                   </div>
+               </div>
+           </div>
+       @else
+           <input type="hidden" id="has_teaser" value="0"/>
+       @endif
+        		<script type="text/javascript">
+                    $(document).ready(function() {
+
+                    var tsr = $('#has_teaser').val();
+                    if(tsr){
+                        setTimeout($('#popup').show(), 7000);
+                    }
+
+                    });
+
+
+
+
+                  </script>
 
 @endsection
 
