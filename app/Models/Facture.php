e86b896a11f31d197268aad3eae09694aef278f3
Clement <clementessomba@gmail.com>
2019-12-09 00:54:45 +0100

    Ajouts

--- app/Models/Facture.php
+++ app/Models/Facture.php
@@ -20,17 +20,41 @@ class Facture extends Model
 	}
 
 	public function lignes(){
-		return $this->hasMany('App\Models\Paiement');
+		if($this->apporteur){
+			return $this->hasMany('App\Models\Paiement','facture_apporteur_id');
+		}
+		if($this->consultant){
+			return $this->hasMany('App\Models\Paiement','facture_consultant_id');
+		}else{
+
+			return $this->hasMany('App\Models\Paiement','facture_alliages_id');
+		}
+
 	}
 
-	public function getMontantapporteurAttribute(){
+	public function getMontantAttribute(){
 		$facture = Facture::find($this->id);
 		$lignes = $facture->lignes;
 		$s=0;
 		if($lignes){
-			foreach($lignes as $ligne){
-				$s = $s+$ligne->montant_apporteur;
+			if($this->apporteur){
+				foreach($lignes as $ligne){
+					$s = $s+$ligne->montant_apporteur;
+				}
 			}
+
+			if($this->consulatnt){
+				foreach($lignes as $ligne){
+					$s = $s+$ligne->montant_consultant;
+				}
+			}
+
+			if($this->alliages){
+				foreach($lignes as $ligne){
+					$s = $s+$ligne->montant_alliages;
+				}
+			}
+
 		}
 	}
 
