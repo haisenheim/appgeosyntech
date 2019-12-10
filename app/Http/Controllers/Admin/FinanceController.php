45e1b78d9817702da7d883554672cbc7a9eb7f3e
Clement <clementessomba@gmail.com>
2019-12-10 09:00:21 +0100

    Ajouts

--- app/Http/Controllers/Admin/FinanceController.php
+++ app/Http/Controllers/Admin/FinanceController.php
@@ -141,7 +141,7 @@ class FinanceController extends Controller
 	}
 
 	public function test(){
-		dd(Projet::with('moi'));
+		dd(Projet::with('moi')->get());
 		$projets = Projet::all();
 		$projets = $projets->sortByDesc('moi_id');
 		$projets = $projets->groupBy(['annee',function($item){
