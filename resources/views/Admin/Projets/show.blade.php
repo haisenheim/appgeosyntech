d2c80186950dd9da64759febb91e19abb0a76069
Clement <clementessomba@gmail.com>
2019-12-11 19:02:49 +0100

    Ajouts

--- resources/views/Admin/Projets/show.blade.php
+++ resources/views/Admin/Projets/show.blade.php
@@ -1567,8 +1567,9 @@
                   @endif
               </div>
               <h3 class="text-primary"> {{$projet->name}}</h3>
+              @if($projet->modele)
                 <button data-target="#meModal" data-toggle="modal" class="btn btn-sm btn-block btn-outline-success">Modèle économique</button>
-
+              @endif
               <br>
               <div class="text-muted">
                 <p class="text-sm">Porteur de projet:
@@ -1619,21 +1620,81 @@
         <!-- /.card-body -->
       </div>
 
-<div class="modal fade" id="meModal">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header bg-success">
-                <h6  class="modal-title text-center">Description du modèle économique</h6>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-            <div class="modal-body">
-                <p><?= $projet->description_modele_economique ?></p>
-            </div>
+
+        @if($projet->modele)
+        <div class="modal fade" id="meModal" tabindex="-1" role="dialog" aria-labelledby="addModalLabel">
+        		<div class="modal-dialog modal-xl" role="document">
+        			<div class="modal-content">
+        				<div class="modal-header bg-success">
+        					<h5 style="text-transform: uppercase; background-color: transparent" class="modal-title" id="myModalLabel"><span>MODELE ECONOMIQUE</span></h5>
+        					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                            </button>
+        				</div>
+        				<div class="modal-body">
+
+        				    <div class="card">
+        				        <div class="card-header">
+
+
+                                      <div class="card-tools">
+
+                                           <button type="button" class="btn btn-tool" data-card-widget="maximize" data-toggle="tooltip" title="Agrandir"><i class="fas fa-expand"></i>
+                                           </button>
+
+                                      </div>
+                                </div>
+        				        <div class="card-body">
+        				        <input type="hidden" name="token" value="{{ $projet->modele->token }}"/>
+        				           <div class="form-group">
+                                       <label for="offre">OFFRE</label>
+                                       <textarea name="offre" id="offre" cols="30" rows="3" class="form-control">{{ $projet->modele->offre }}</textarea>
+                                   </div>
+                                   <div class="form-group">
+                                       <label for="segment">SEGMENTS DE CLIENTELE</label>
+                                       <textarea name="segment" id="segment" cols="30" rows="3" class="form-control">{{ $projet->modele->segment }}</textarea>
+                                   </div>
+                                   <div class="form-group">
+                                       <label for="canaux">CANAUX DE DISTRIBUTION </label>
+                                       <textarea name="canaux" id="canaux" cols="30" rows="3" class="form-contol">{{ $projet->modele->canaux }}</textarea>
+                                   </div>
+                                   <div class="form-group">
+                                       <label for="relation">RELATION CLIENTS </label>
+                                       <textarea name="relation" id="relation" cols="30" rows="3" class="form-control">{{ $projet->modele->relation }}</textarea>
+                                   </div>
+                                   <div class="form-group">
+                                       <label for="partenaires">PARTENAIRES CLES</label>
+                                       <textarea name="partenaires" id="partenaires" cols="30" rows="3" class="form-control">{{ $projet->modele->partenaires }}</textarea>
+                                   </div>
+                                   <div class="form-group">
+                                       <label for="activites">ACTIVITES CLES </label>
+                                       <textarea name="activites" id="activites" cols="30" rows="3" class="form-control">{{ $projet->modele->activites }}</textarea>
+                                   </div>
+                                   <div class="form-group">
+                                       <label for="ressources">RESSOURCES CLES </label>
+                                       <textarea name="ressources" id="ressources" cols="30" rows="3" class="form-control">{{ $projet->modele->ressources }}</textarea>
+                                   </div>
+                                   <div class="form-group">
+                                       <label for="couts">COUTS</label>
+                                       <textarea name="couts" id="couts" cols="30" rows="3" class="form-control">{{ $projet->modele->couts }}</textarea>
+                                   </div>
+                                   <div class="form-group">
+                                       <label for="revenus">REVENUS</label>
+                                       <textarea name="revenus" id="revenus" cols="30" rows="3" class="form-control">{{ $projet->modele->revenus }}</textarea>
+                                   </div>
+        				        </div>
+        				        <div class="card-footer">
+        				           <button class="btn btn-block btn-success" type="submit">ENREGISTRER</button>
+        				        </div>
+        				    </div>
+
+        				</div>
+        			</div>
+
+        		</div>
+
         </div>
-    </div>
-</div>
+        @endif
 
 
 
