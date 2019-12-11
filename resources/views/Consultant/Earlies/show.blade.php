5a3505f7eba662cc09e5030c93944274b91d2d3a
Clement <clementessomba@gmail.com>
2019-12-11 21:39:59 +0100

    Ajouts

--- resources/views/Consultant/Earlies/show.blade.php
+++ resources/views/Consultant/Earlies/show.blade.php
@@ -48,9 +48,7 @@
                             <p><i class="fa fa-map-marker"></i> {{ $projet->ville->name }}</p>
                             <input type="hidden" id="pl_id" value="{{ $projet->plan_id }}"/>
 
-                            @if($projet->modele)
-                                <button data-target="#meModal" data-toggle="modal" class="btn btn-sm btn-block btn-outline-success">Modèle économique</button>
-                             @endif
+
                             <button data-toggle="modal" data-target="#synDiagIntModal" class="btn btn-sm btn-outline-info btn-block"><i class="fa fa-pencil"></i>SYNTHESE DU DIAGNOSTIC INTERNE</button>
 
 
@@ -1869,6 +1867,138 @@
         		</div>
         	</form>
         </div>
+
+        @if($projet->modele)
+        <style>
+            #meModal .card-title{
+                font-weight: 800;
+                font-size: 0.9rem;
+            }
+        </style>
+        <div class="modal fade" id="meModal" tabindex="-1" role="dialog" aria-labelledby="addModalLabel">
+        		<div class="modal-dialog modal-xl" role="document">
+        			<div class="modal-content">
+        				<div class="modal-header bg-success">
+        					<h5 style="text-transform: uppercase; background-color: transparent" class="modal-title" id="myModalLabel"><span>MODELE ECONOMIQUE</span></h5>
+        					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                            </button>
+        				</div>
+        				<div class="modal-body">
+
+        				    <div class="card">
+        				        <div class="card-header">
+
+
+                                      <div class="card-tools">
+
+                                           <button type="button" class="btn btn-tool" data-card-widget="maximize" data-toggle="tooltip" title="Agrandir"><i class="fas fa-expand"></i>
+                                           </button>
+
+                                      </div>
+                                </div>
+        				        <div class="card-body">
+        				            <div class="row">
+        				                <div class="col-md-2 col-sm-12">
+        				                    <div class="card">
+        				                        <div class="card-header">
+        				                            <h3 class="card-title">PARTENAIRES</h3>
+        				                        </div>
+        				                        <div class="card-body">
+        				                            <?= $projet->modele->partenaires ?>
+        				                        </div>
+        				                    </div>
+        				                </div>
+        				                <div class="col-md-3 col-sm-12">
+        				                    <div class="card">
+        				                        <div class="card-header">
+        				                            <h3 class="card-title">ACTIVITES</h3>
+        				                        </div>
+        				                        <div class="card-body">
+        				                            <?= $projet->modele->activites ?>
+        				                        </div>
+        				                    </div>
+        				                    <div class="card">
+        				                        <div class="card-header">
+        				                            <h3 class="card-title">RESSOURCES</h3>
+        				                        </div>
+        				                        <div class="card-body">
+        				                            <?= $projet->modele->ressources ?>
+        				                        </div>
+        				                    </div>
+        				                </div>
+        				                <div class="col-md-2 col-sm-12">
+        				                    <div class="card">
+        				                        <div class="card-header">
+        				                            <h3 class="card-title">OFFRE</h3>
+        				                        </div>
+        				                        <div class="card-body">
+        				                            <?= $projet->modele->offre ?>
+        				                        </div>
+        				                    </div>
+        				                </div>
+        				                <div class="col-md-3 col-sm-12">
+        				                    <div class="card">
+        				                        <div class="card-header">
+        				                            <h3 class="card-title">RELATION CLIENT</h3>
+        				                        </div>
+        				                        <div class="card-body">
+        				                            <?= $projet->modele->relation ?>
+        				                        </div>
+        				                    </div>
+        				                    <div class="card">
+        				                        <div class="card-header">
+        				                            <h3 class="card-title">CANAUX DE DISTRIBUTION</h3>
+        				                        </div>
+        				                        <div class="card-body">
+        				                            <?= $projet->modele->canaux ?>
+        				                        </div>
+        				                    </div>
+        				                </div>
+        				                <div class="col-md-2 col-sm-12">
+        				                    <div class="card">
+        				                        <div class="card-header">
+        				                            <h3 class="card-title">SEGMENTS CLIENT</h3>
+        				                        </div>
+        				                        <div class="card-body">
+        				                            <?= $projet->modele->segment ?>
+        				                        </div>
+        				                    </div>
+        				                </div>
+        				            </div>
+        				            <div class="row">
+                                        <div class="col-md-6 col-sm-12">
+                                            <div class="card">
+        				                        <div class="card-header">
+        				                            <h3 class="card-title">STRUCTURE DES COUTS</h3>
+        				                        </div>
+        				                        <div class="card-body">
+        				                            <?= $projet->modele->couts ?>
+        				                        </div>
+        				                    </div>
+                                        </div>
+                                        <div class="col-md-6 col-sm-12">
+                                            <div class="card">
+        				                        <div class="card-header">
+        				                            <h3 class="card-title">SOURCES DE REVENUS</h3>
+        				                        </div>
+        				                        <div class="card-body">
+        				                            <?= $projet->modele->revenus ?>
+        				                        </div>
+        				                    </div>
+                                        </div>
+        				            </div>
+
+        				    </div>
+
+        				</div>
+        			</div>
+
+        		</div>
+
+        </div>
+
+     @endif
 
 <style>
          div.note-editor.note-frame{
