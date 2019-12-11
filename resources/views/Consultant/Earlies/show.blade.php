cc29f6a89212239966589bdd41b52462fb57dd91
Clement <clementessomba@gmail.com>
2019-12-11 21:23:26 +0100

    Ajouts

--- resources/views/Consultant/Earlies/show.blade.php
+++ resources/views/Consultant/Earlies/show.blade.php
@@ -1848,7 +1848,7 @@
      @endif
 
     <!-- Edition de la synthese du diagnostic interne -->
-    <div class="modal" id="synDiagIntModal" tabindex="-1" role="dialog" aria-labelledby="ad">
+    <div class="modal fade" id="synDiagIntModal" tabindex="-1" role="dialog" aria-labelledby="ad">
 
     		<div class="modal-dialog modal-lg" role="document">
     			<div class="modal-content">
@@ -1862,7 +1862,18 @@
 
     				<div class="modal-body">
     				<div class="card">
-
+    				    <div class="card-body">
+    				        <form method="post" action="/consultant/projet/synthese1">
+                                <input type="hidden" id="" name="projet_token" value="<?= $projet->token ?>" />
+                                {{csrf_field()}}
+                                <div class="form-group">
+                                    <textarea class="form-control"  name="synthese1" id="synthese1" cols="30" rows="10" >{{ $projet->synthese_diagnostic_interne }}</textarea>
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-danger btn-block"><i class="fa fa-save"></i> ENREGISTRER</button>
+                                </div>
+                            </form>
+    				    </div>
     				</div>
 
     				</div>
