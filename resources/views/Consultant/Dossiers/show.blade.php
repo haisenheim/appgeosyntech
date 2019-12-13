561d8e1a1e3d83d6f7760c1cab559a058fc56d94
Clement <clementessomba@gmail.com>
2019-12-13 08:32:11 +0100

    Ajouts

--- resources/views/Consultant/Dossiers/show.blade.php
+++ resources/views/Consultant/Dossiers/show.blade.php
@@ -2029,7 +2029,7 @@
     		{{csrf_field()}}
     		<div class="modal-dialog modal-lg" role="document">
     			<div class="modal-content">
-    			        <div class="modal-header">
+    			        <div class="modal-header bg-warning">
                             <h6  class="modal-title text-center">SYNTHESE DU DIAGNOSTIC INTERNE</h6>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                               <span aria-hidden="true">&times;</span>
@@ -2042,7 +2042,7 @@
     					</div>
     				</div>
     				<div class="modal-footer">
-    					<button type="submit" class="btn btn-danger"><i class="fa fa-save"></i> ENREGISTRER</button>
+    					<button type="submit" class="btn btn-outline-success btn-block"><i class="fa fa-save"></i> ENREGISTRER</button>
     				</div>
 
     			</div>
@@ -2071,7 +2071,7 @@
         					</div>
         				</div>
         				<div class="modal-footer">
-        					<button type="submit" class="btn btn-danger"><i class="fa fa-save"></i> ENREGISTRER</button>
+        					<button type="submit" class="btn btn-outline-info btn-block"><i class="fa fa-save"></i> ENREGISTRER</button>
         				</div>
 
         			</div>
@@ -2088,17 +2088,20 @@
         		{{csrf_field()}}
         		<div class="modal-dialog modal-lg" role="document">
         			<div class="modal-content">
-        				<div class="modal-header">
-        					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        					<h5 style="text-transform: uppercase; background-color: transparent" class="modal-title" id="myModalLabel"><span> SYNTHESE DU DIAGNOSTIC STRATEGIQUE</span></h5>
-        				</div>
+        			    <div class="modal-header bg-primary">
+                          <h6  class="modal-title text-center">SYNTHESE DU DIAGNOSTIC STRATEGIQUE</h6>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+
         				<div class="modal-body">
         					<div class="form-group">
         						<textarea class="form-control"  name="synthese3" id="synthese3" cols="30" rows="10" >{{ $projet->synthese_diagnostic_strategique }}</textarea>
         					</div>
         				</div>
         				<div class="modal-footer">
-        					<button type="submit" class="btn btn-danger"><i class="fa fa-save"></i> ENREGISTRER</button>
+        					<button type="submit" class="btn btn-outline-primary btn-block"><i class="fa fa-save"></i> ENREGISTRER</button>
         				</div>
 
         			</div>
@@ -2127,56 +2130,56 @@
                                 <legend style="">Présentation de l’entreprise et du porteur de projet </legend>
                                 <div class="form-group">
                                     <label for="contexte">PRESENTATION DE L'ENTREPRISE</label>
-                                    <textarea class="form-control" name="entreprise" rows="3" id="contexte"></textarea>
+                                    <textarea class="form-control" name="entreprise" rows="3" id="contexte">{{ $projet->teaser?$projet->teaser->entreprise:'' }}</textarea>
                                 </div>
 
                                 <div class="form-group">
                                     <label for="youtube">LIEN YOUTUBE</label>
-                                    <input class="form-control" type="text" name="youtubeUri"/>
+                                    <input class="form-control" type="text" name="youtubeUri" id="youtube" value="{{ $projet->teaser?$projet->teaser->youtubeUri:'' }}"/>
                                 </div>
 
                                 <div class="form-group">
                                     <label for="contexte">PRESENTATION DU PORTEUR DE PROJET</label>
-                                    <textarea class="form-control" name="porteur" rows="3" id="contexte"></textarea>
+                                    <textarea class="form-control" name="porteur" rows="3" id="contexte">{{ $projet->teaser?$projet->teaser->porteur:'' }}</textarea>
                                 </div>
                             </fieldset>
 
                             <div class="form-group">
                                 <label for="problematique">Problématique à résoudre </label>
-                                <textarea name="problematique" id="problematique"></textarea>
+                                <textarea name="problematique" id="problematique">{{ $projet->teaser?$projet->teaser->problematique:'' }}</textarea>
                             </div>
                             <div class="form-group">
                                 <label for="solution">Solution proposée  </label>
-                                <textarea name="solution" id="solution"></textarea>
+                                <textarea name="solution" id="solution">{{ $projet->teaser?$projet->teaser->solution:'' }}</textarea>
                             </div>
 
                             <div class="form-group">
                                 <label for="concurrence">Présentation de la concurrence    </label>
-                                <textarea name="concurrence" id="concurrence"></textarea>
+                                <textarea name="concurrence" id="concurrence">{{ $projet->teaser?$projet->teaser->concurrence:'' }}</textarea>
                             </div>
 
                             <div class="form-group">
                                 <label for="va">Valeur ajoutée apportée par rapport aux concurrents   </label>
-                                <textarea name="va" id="va"></textarea>
+                                <textarea name="va" id="va">{{ $projet->teaser?$projet->teaser->va:'' }}</textarea>
                             </div>
 
                             <div class="form-group">
                                 <label for="realisations">Ce qui a déjà été fait   </label>
-                                <textarea name="realisations" id="realisations"></textarea>
+                                <textarea name="realisations" id="realisations">{{ $projet->teaser?$projet->teaser->realisations:'' }}</textarea>
                             </div>
 
                             <div class="form-group">
                                 <label for="objectifs">Présentation des objectifs   </label>
-                                <textarea name="objectifs" id="objectifs"></textarea>
+                                <textarea name="objectifs" id="objectifs">{{ $projet->teaser?$projet->teaser->objectifs:'' }}</textarea>
                             </div>
 
                             <div class="form-group">
                                 <label for="besoins">Présentation des besoins financiers   </label>
-                                <textarea name="besoins" id="besoins"></textarea>
+                                <textarea name="besoins" id="besoins">{{ $projet->teaser?$projet->teaser->besoins:'' }}</textarea>
                             </div>
                             <div class="form-group">
                                 <label for="chiffres">Quelques chiffres prévisionnels    </label>
-                                <textarea name="chiffres" id="chiffres"></textarea>
+                                <textarea name="chiffres" id="chiffres">{{ $projet->teaser?$projet->teaser->chiffres:'' }}</textarea>
                             </div>
         				</div>
         				<div class="modal-footer">
