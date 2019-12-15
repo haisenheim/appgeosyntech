85ee396992586aae50b1ee2d4348e42c1b4e3c5d
Clement <clementessomba@gmail.com>
2019-12-15 10:42:27 +0100

    Ajouts

---
+++ resources/views/includes/Sidebars/dossier.blade.php
@@ -0,0 +1,108 @@
+<?php use \Illuminate\Support\Facades\Auth; ?>
+ <div class="card">
+    <div class="card-body">
+
+
+    <div style="height: 300px; width: 100%; background: url('{{ $projet->imageUri?asset('img/'.$projet->imageUri):asset('img/logo.png') }}'); background-size: cover ">
+
+    </div>
+
+    <p>CODE : {{ $projet->code }}</p>
+    <p>DATE DE CREATION : <span class="value"> {{ date_format($projet->created_at,'d/m/Y') }}</span></p>
+    <?php if(Auth::user()->role_id==2): ?>
+        <p>PROMOTEUR : <span class="value">{{ $projet->owner->name }}</span></p>
+    <?php endif; ?>
+
+    @if($projet->modele)
+        <a href="#" data-toggle="modal" data-target="#bm" class="btn btn-outline-success btn-block">MODELE ECONOMIQUE</a>
+    @endif
+    @if($projet->etape>=4)
+        <ul class="list-group">
+            <li class="list-group-item">MONTANT DES INVESTISSEMENT : <span class="value"><?= $projet->montant_investissement ?></span></li>
+            <li class="list-group-item">BESOIN EN FONDS DE ROULEMENT : <span class="value"><?= $projet->bfr ?></span></li>
+            <li style="font-weight: bold;" class="list-group-item">COUT GLOBAL DU PROJET : <span class="value"><?= number_format(($projet->montant_investissement + $projet->bfr),0,',','.') ?> <sup><?= $projet->devise->abb ?></sup></span></li>
+        </ul>
+
+        <fieldset>
+            <legend>MOYENS DE FINANCEMENT</legend>
+            <ul class="list-group">
+                @if($projet->moyens)
+                    @foreach($projet->financements as $moyen)
+
+                        <li class="list-group-item"><?= $moyen->moyen? $moyen->moyen->name:'-' ?> : <span class="value"><?= $moyen->montant ?></span></li>
+                    @endforeach
+                @endif
+            </ul>
+        </fieldset>
+
+    @endif
+    <input type="hidden" id="id" value="<?= $projet->token ?>"/>
+    <p><i class="fa fa-map-marker"></i> {{ $projet->ville->name }}</p>
+    <input type="hidden" id="pl_id" value="{{ $projet->plan_id }}"/>
+
+
+    <button data-toggle="modal" data-target="#synDiagIntModal" class="btn btn-outline-warning btn-block btn-xs"><i class="fa fa-pencil"></i>SYNTHESE DU DIAGNOSTIC INTERNE</button>
+
+
+    @if($projet->etape>=2)
+        <button style="margin-top: 7px" data-toggle="modal" data-target="#synDiagExtModal" class="btn btn-outline-info btn-block btn-xs"><i class="fa fa-pencil"></i>SYNTHESE DU DIAGNOSTIC EXTERNE</button>
+    @endif
+
+    @if($projet->etape>=3)
+        <button style="margin-top: 7px" data-toggle="modal" data-target="#synDiagStratModal" class="btn btn-outline-primary btn-block btn-xs"><i class="fa fa-pencil"></i>SYNTHESE DU DIAGNOSTIC STRATEGIQUE</button>
+    @endif
+
+     @if($projet->etape>=4)
+        <button style="margin-top: 7px" data-toggle="modal" data-target="#teaserModal" class="btn btn-outline-success btn-block btn-xs"><i class="fa fa-pencil"></i> EDITER LE TEASER</button>
+    @endif
+
+
+    @if($projet->modepaiement_id>0)
+        <h6 class="page-header" style="background-color: inherit">MODALITE DE PAIEMENT</h6>
+        <ul class="list-group">
+            <li class="list-group-item"><span style="font-weight: 700">{{ $projet->modepaiement->name }}</span></li>
+
+            <li class="list-group-item">PRIX TTC : <span style="font-weight: 700">{{ number_format($projet->traite * 4 ,0,',','.') }} <sup>{{ $projet->devise->abb }}</sup></span>({{ number_format($projet->traite ,0,',','.') }} <sup>{{ $projet->devise->abb }}</sup>/ETAPE)</li>
+        </ul>
+    @endif
+
+    @if($projet->modepaiement_id==0)
+
+       <?php if(Auth::user()->role_id==2): ?>
+            <div>
+                <h6 class="page-header" style="background-color: inherit">FORMULE DE PAIEMENT</h6>
+
+                <hr/>
+                <form action="/consultant/dossier/add-mode" method="get" class="form-inline">
+                    <input type="hidden" id="projet_token" value="<?= $projet->token ?>" name="projet_token"/>
+                    <select style="background-color: #FFFFFF" class="form-control" name="mode_id" id="mode_id">
+                        <option value="0">Choix d'une offre de service</option>
+                        @foreach($modes as $mode)
+                            <option value="{{ $mode->id }}">{{ $mode->name }}</option>
+                        @endforeach
+                    </select>
+                    <button class="btn btn-xs btn-danger" type="submit"><i class="fa fa-check"></i> ENREGISTRER</button>
+
+                </form>
+            </div>
+        <?php endif; ?>
+
+        <?php if(Auth::user()->role_id==3): ?>
+            <div>
+                <h6 class="page-header" style="background-color: inherit">FORMULE DE PAIEMENT</h6>
+
+                    <b>AUCUN CHOIX POUR L'INSTANT</b>
+                    <P>VOUS AVEZ A CHOISIR ENTRE DEUX FORMULES</P>
+                    <ul>
+                        <li>GOLD (ETUDE DE MARCHE OFFERTE)</li>
+                        <li>SILVER (SANS ETUDE DE MARCHE)</li>
+                    </ul>
+                    <b>Contactez votre expert OBAC pour en savoir davantage</b>
+
+            </div>
+        <?php endif; ?>
+
+    @endif
+
+</div>
+</div>
