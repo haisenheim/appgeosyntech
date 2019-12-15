9914c68e95f40142763360ac5d670837942e97fd
Clement <clementessomba@gmail.com>
2019-12-15 15:07:18 +0100

    Ajouts

--- resources/views/includes/Sidebars/dossier.blade.php
+++ resources/views/includes/Sidebars/dossier.blade.php
@@ -12,9 +12,9 @@
         <p>PROMOTEUR : <span class="value">{{ $projet->owner->name }}</span></p>
     <?php endif; ?>
 
-    @if($projet->modele)
+
         <a href="#" data-toggle="modal" data-target="#bm" class="btn btn-outline-success btn-block">MODELE ECONOMIQUE</a>
-    @endif
+
     @if($projet->etape>=4)
         <ul class="list-group">
             <li class="list-group-item">MONTANT DES INVESTISSEMENT : <span class="value"><?= $projet->montant_investissement ?></span></li>
