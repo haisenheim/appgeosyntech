7928290a0d896c1e83453c931ceed023bef64020
Clement <clementessomba@gmail.com>
2019-12-21 18:09:16 +0100

    Ajouts

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +17,31 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+	    /*if(!Collection::hasMacro('paginate')){
+
+	    }
+	    Collection::macro('paginate', function($perPage, $total = null, $page = null, $pageName = 'page') {
+		    $page = $page ?: LengthAwarePaginator::resolveCurrentPage($pageName);
+		    return new LengthAwarePaginator(
+			    $this->forPage($page, $perPage),
+			    $total ?: $this->count(),
+			    $perPage,
+			    $page,
+			    [
+				    'path' => LengthAwarePaginator::resolveCurrentPath(),
+				    'pageName' => $pageName,
+			    ]
+		    );
+	    });*/
+
+	    if (!Collection::hasMacro('paginate')) {
+		    Collection::macro('paginate',
+			    function ($perPage = 15, $page = null, $options = []) {
+				    $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
+				    return (new LengthAwarePaginator($this->forPage($page, $perPage), $this->count(), $perPage, $page, $options))
+                    ->withPath('');
+            });
+	    }
     }
 
     /**
