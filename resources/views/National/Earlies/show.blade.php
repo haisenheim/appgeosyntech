8e3c845f61fe79676cec7f68d68014543702d008
Clement <clementessomba@gmail.com>
2019-12-22 11:53:35 +0100

    ajouts

--- resources/views/National/Earlies/show.blade.php
+++ resources/views/National/Earlies/show.blade.php
@@ -83,129 +83,7 @@
                 font-size: 0.9rem;
             }
         </style>
-        <div class="modal fade" id="meModal" tabindex="-1" role="dialog" aria-labelledby="addModalLabel">
-        		<div class="modal-dialog modal-xl" role="document">
-        			<div class="modal-content">
-        				<div class="modal-header bg-success">
-        					<h5 style="text-transform: uppercase; background-color: transparent" class="modal-title" id="myModalLabel"><span>MODELE ECONOMIQUE</span></h5>
-        					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                              <span aria-hidden="true">&times;</span>
-                            </button>
-        				</div>
-        				<div class="modal-body">
-
-        				    <div class="card">
-        				        <div class="card-header">
-
-
-                                      <div class="card-tools">
-
-                                           <button type="button" class="btn btn-tool" data-card-widget="maximize" data-toggle="tooltip" title="Agrandir"><i class="fas fa-expand"></i>
-                                           </button>
-
-                                      </div>
-                                </div>
-        				        <div class="card-body">
-        				            <div class="row">
-        				                <div class="col-md-2 col-sm-12">
-        				                    <div class="card">
-        				                        <div class="card-header">
-        				                            <h3 class="card-title">PARTENAIRES</h3>
-        				                        </div>
-        				                        <div class="card-body">
-        				                            <?= $projet->modele->partenaires ?>
-        				                        </div>
-        				                    </div>
-        				                </div>
-        				                <div class="col-md-3 col-sm-12">
-        				                    <div class="card">
-        				                        <div class="card-header">
-        				                            <h3 class="card-title">ACTIVITES</h3>
-        				                        </div>
-        				                        <div class="card-body">
-        				                            <?= $projet->modele->activites ?>
-        				                        </div>
-        				                    </div>
-        				                    <div class="card">
-        				                        <div class="card-header">
-        				                            <h3 class="card-title">RESSOURCES</h3>
-        				                        </div>
-        				                        <div class="card-body">
-        				                            <?= $projet->modele->ressources ?>
-        				                        </div>
-        				                    </div>
-        				                </div>
-        				                <div class="col-md-2 col-sm-12">
-        				                    <div class="card">
-        				                        <div class="card-header">
-        				                            <h3 class="card-title">OFFRE</h3>
-        				                        </div>
-        				                        <div class="card-body">
-        				                            <?= $projet->modele->offre ?>
-        				                        </div>
-        				                    </div>
-        				                </div>
-        				                <div class="col-md-3 col-sm-12">
-        				                    <div class="card">
-        				                        <div class="card-header">
-        				                            <h3 class="card-title">RELATION CLIENT</h3>
-        				                        </div>
-        				                        <div class="card-body">
-        				                            <?= $projet->modele->relation ?>
-        				                        </div>
-        				                    </div>
-        				                    <div class="card">
-        				                        <div class="card-header">
-        				                            <h3 class="card-title">CANAUX DE DISTRIBUTION</h3>
-        				                        </div>
-        				                        <div class="card-body">
-        				                            <?= $projet->modele->canaux ?>
-        				                        </div>
-        				                    </div>
-        				                </div>
-        				                <div class="col-md-2 col-sm-12">
-        				                    <div class="card">
-        				                        <div class="card-header">
-        				                            <h3 class="card-title">SEGMENTS CLIENT</h3>
-        				                        </div>
-        				                        <div class="card-body">
-        				                            <?= $projet->modele->segment ?>
-        				                        </div>
-        				                    </div>
-        				                </div>
-        				            </div>
-        				            <div class="row">
-                                        <div class="col-md-6 col-sm-12">
-                                            <div class="card">
-        				                        <div class="card-header">
-        				                            <h3 class="card-title">STRUCTURE DES COUTS</h3>
-        				                        </div>
-        				                        <div class="card-body">
-        				                            <?= $projet->modele->couts ?>
-        				                        </div>
-        				                    </div>
-                                        </div>
-                                        <div class="col-md-6 col-sm-12">
-                                            <div class="card">
-        				                        <div class="card-header">
-        				                            <h3 class="card-title">SOURCES DE REVENUS</h3>
-        				                        </div>
-        				                        <div class="card-body">
-        				                            <?= $projet->modele->revenus ?>
-        				                        </div>
-        				                    </div>
-                                        </div>
-        				            </div>
-
-        				    </div>
-
-        				</div>
-        			</div>
-
-        		</div>
-
-        </div>
-        </div>
+        @include('includes.Show.business_model')
      @endif
 
 
@@ -504,6 +382,10 @@
 <script src="{{asset('plugins/datatables/jquery.dataTables.js') }}"></script>
 <script src="{{asset('plugins/datatables-bs4/js/dataTables.bootstrap4.js')}}"></script>
 <script>
+    $(document).ready(function(){
+        var h2 = $('#side2 .card').height();
+        $('#side1 .card').height(h2).css({'overflow-y':'scroll'});
+    });
   $(function () {
     $("#table-invest").DataTable({
         "lengthChange":true
