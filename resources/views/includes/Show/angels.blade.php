def632b89bcf60362758f590ca22ac588295a9ef
Clement <clementessomba@gmail.com>
2019-12-22 18:53:49 +0100

    ajouts

--- resources/views/includes/Show/angels.blade.php
+++ resources/views/includes/Show/angels.blade.php
@@ -87,7 +87,7 @@
                                                  @endif
                                                   @if($invest->dossier->owner_id== \Illuminate\Support\Facades\Auth::user()->id)
                                                         <?php if($invest->lettre): ?>
-                                                            <a class="dropdown-item" href="/national/letter/create/{{ $invest->token }}">Lettre d'intention</a>
+                                                            <a class="dropdown-item" href="/owner/letter/create/{{ $invest->token }}">Lettre d'intention</a>
                                                           <?php endif; ?>
                                                       <?php if(!$invest->doc_juridique): ?>
                                                         <a title="Autoriser l'accès à la documentation juridique" class="dropdown-item" href="/national/projet/docs/open/{{ $invest->token }}">Ouvrir la documentation</a>
