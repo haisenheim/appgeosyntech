0efb710e9d860e8bd80a2e79d6a2080f21db74f5
Clement <clementessomba@gmail.com>
2019-12-22 15:58:18 +0100

    ajouts

--- app/Http/Controllers/National/ProjetController.php
+++ app/Http/Controllers/National/ProjetController.php
@@ -247,6 +247,7 @@ class ProjetController extends Controller
 
 	public function createLetter($token){
 		$investissement = Investissement::where('token',$token)->first();
+		dd($investissement);
 		return view('National/lettre_intention')->with(compact($investissement));
 	}
 
