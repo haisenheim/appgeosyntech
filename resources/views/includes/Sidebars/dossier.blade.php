8000ba59ea8669fe55c78eed040981aacff0403d
Clement <clementessomba@gmail.com>
2019-12-25 00:24:11 +0100

    Ajouts

--- resources/views/includes/Sidebars/dossier.blade.php
+++ resources/views/includes/Sidebars/dossier.blade.php
@@ -41,7 +41,7 @@
     <p><i class="fa fa-map-marker"></i> {{ $projet->ville->name }}</p>
     <input type="hidden" id="pl_id" value="{{ $projet->plan_id }}"/>
 
-    @if($projet->synthese_diagnostic_interne)
+    @if($projet->etape>=1)
         <button data-toggle="modal" data-target="#synDiagIntModal" class="btn-synt btn btn-outline-warning btn-block btn-xs"><i class="fa fa-pencil"></i>SYNTHESE DU DIAGNOSTIC INTERNE</button>
     @endif
 
@@ -95,7 +95,7 @@
                     <b>AUCUN CHOIX POUR L'INSTANT</b>
                     <P>VOUS AVEZ A CHOISIR ENTRE DEUX FORMULES</P>
                     <ul>
-                        <li>GOLD (ETUDE DE MARCHE OFFERTE)</li>
+                        <li>GOLD (AVEC ETUDE DE MARCHE)</li>
                         <li>SILVER (SANS ETUDE DE MARCHE)</li>
                     </ul>
                     <b>Contactez votre expert OBAC pour en savoir davantage</b>
