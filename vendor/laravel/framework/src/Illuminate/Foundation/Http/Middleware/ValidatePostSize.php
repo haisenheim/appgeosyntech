3ae422ea528d370a5eebd1d55aefaee763e417b9
Clement <clementessomba@gmail.com>
2020-01-05 23:37:34 +0100

    ajout

--- vendor/laravel/framework/src/Illuminate/Foundation/Http/Middleware/ValidatePostSize.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/Http/Middleware/ValidatePostSize.php
@@ -34,7 +34,7 @@ class ValidatePostSize
      */
     protected function getPostMaxSize()
     {
-	    dd(ini_get('post_max_size'));
+	   // dd(ini_get('post_max_size'));
         if (is_numeric($postMaxSize = ini_get('post_max_size'))) {
             return (int) $postMaxSize;
         }
