d8ee6a16f9161b37b2f4b17e482eefa29e714d50
Clement <clementessomba@gmail.com>
2020-01-15 10:27:58 +0100

    ajouts

--- resources/views/Consultant/Agenda/index.blade.php
+++ resources/views/Consultant/Agenda/index.blade.php
@@ -36,7 +36,7 @@ MON AGENDA
    <script src="{{ asset('plugins/fullcalendar/main.min.js') }}"></script>
    <script src="{{ asset('plugins/fullcalendar-daygrid/main.min.js') }}"></script>
    <script src="{{ asset('plugins/fullcalendar-timegrid/main.min.js') }}"></script>
-   <script src="{{ asset('plugins/fullcalendar-interaction/main.min.js') }}"></script>
+
    <script src="{{ asset('plugins/fullcalendar-bootstrap/main.min.js')}}"></script>
 
 
