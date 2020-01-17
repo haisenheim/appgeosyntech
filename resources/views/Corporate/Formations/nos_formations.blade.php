e5297edb9f8d9fd2ba607da6537aaa0804e9a264
Clement <clementessomba@gmail.com>
2020-01-17 12:18:12 +0100

    ajouts

--- resources/views/Corporate/Formations/nos_formations.blade.php
+++ resources/views/Corporate/Formations/nos_formations.blade.php
@@ -1,7 +1,7 @@
 @extends('......layouts.corporate')
 @section('content')
 
-<?php dd($entreprise->inscriptions) ?>
+
 
  <div class="card">
 
@@ -18,22 +18,20 @@
                       </th>
                       <th>Cout en ligne</th>
                       <th>Cout en présentiel</th>
-                      <th>
-                          Contributeur
-                      </th>
 
+                      <th>Nb. d'inscrits</th>
                       <th>Nb. Modules</th>
                       <th></th>
                   </tr>
               </thead>
               <tbody>
-                   @foreach($formations as $formation)
+                   @foreach($entreprise->inscriptions as $inscription)
+                        <?php $formation = $inscription->formation ?>
                         <tr>
                             <td>#</td>
                             <td>
                             <span class="text-bold text-lg-left">{{ $formation->name }}</span>- <small>{{ $formation->created_at?date_format($formation->created_at,'d/m/Y'):'' }}</small><br/>
-                            <?= $formation->active?'<span class="badge badge-success">ACTIVE</span>':'<span class="badge badge-danger">Bloquée</span>' ?> -
-                            <?= $formation->free?'<span class="badge badge-info">GRATUITE</span>':'<span class="badge badge-warning">PAYANTE</span>' ?>- <?= $formation->interne?'<span class="badge badge-success"><i class="fa fa-users"></i> Consultants </span>':'<span class="badge badge-warning"><i class="fa fa-school"></i> Ecoles</span>' ?>
+
                             </td>
                             <td>
                                 {{number_format($formation->prix_ligne,0,',','.')}}
@@ -41,19 +39,16 @@
                             <td>
                                 {{number_format($formation->prix_presentiel,0,',','.')}}
                             </td>
+                            <td>
+                                {{ $inscription->comptes->count() }}
+                            </td>
 
-                            <td>{{$formation->contributeur?$formation->contributeur->name:'-'}}</td>
+
                             <td>{{ $formation->modules->count() }}</td>
 
                           <td class="project-actions text-right">
                                 <ul>
                                     <li class="list-inline-item" title="Afficher"> <a class="btn btn-info btn-xs" href="/corporate/formations/{{ $formation->token  }}"><i class="fas fa-search"></i></a></li>
-
-                                           @if($formation->active)
-                                               <li title="bloquer cette formation" class="list-inline-item"><a class="btn btn-danger btn-xs" href="/corporate/formation/disable/{{ $formation->token }}"><i class="fa fa-lock"></i></a></li>
-                                           @else
-                                             <li title="débloquer ce formation" class="list-inline-item"><a class="btn btn-success btn-xs" href="/corporate/formation/enable/{{ $formation->token }}"><i class="fa fa-unlock"></i></a></li>
-                                           @endif
                                 </ul>
                            </td>
                         </tr>
