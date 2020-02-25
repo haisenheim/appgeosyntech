532bdf83722d12193a18f76c488938caee4b3f4a
Clement <clementessomba@gmail.com>
2020-02-25 17:37:36 +0100

    Ajouts

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,92 +1,101 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 3 | Log in</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+<!doctype html>
+<html lang="en">
 
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="<?= asset('/plugins/fontawesome-free/css/all.min.css') ?>">
+    <head>
+        <meta charset="utf-8" />
+        <title>OTC | OBAC TRAINING CENTER</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content="Themesdesign" name="author" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="{{ asset('assets/img/favicon.ico') }}">
 
-  <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="<?= asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css') ?>">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="<?= asset('dist/css/adminlte.min.css') ?>">
-  <!-- Google Font: Source Sans Pro -->
+        <!-- Bootstrap Css -->
+        <!-- Bootstrap Css -->
+        <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+        <!-- Icons Css -->
+        <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+        <!-- App Css-->
+        <link href="{{ asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
 
-</head>
-<body style="background: url('<?= asset('img/logo-obac.png') ?>'); background-size: cover" class="hold-transition login-page">
-<div class="login-box" style="background: #FFFFFF">
-  <div class="login-logo">
-        <div style="width: 200px; margin: 15px auto;">
-            <img src="<?= asset('img/logo-obac.png') ?>" alt="" style="width: 150px; height: 150px; border-radius: 50%"/>
+    </head>
+
+    <body class="bg-primary bg-pattern">
+        <div class="home-btn d-none d-sm-block">
+            <a href="/"><i class="mdi mdi-home-variant h2 text-white"></i></a>
         </div>
-  </div>
-  <!-- /.login-logo -->
-  <div class="card" style="border: 1px solid #007bff">
-    <div style="border-radius: 5%;" class="card-body login-card-body">
-      <p class="login-box-msg">ACCEDEZ A VOTRE ESPACE</p>
 
-      <form action="{{ route('login') }}" method="post">
-       {{ csrf_field() }}
-        <div class="input-group mb-3">
-          <input type="email" class="form-control" name="email" placeholder="Email">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
+        <div class="account-pages my-5 pt-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="text-center mb-5">
+                            <a href="/" class="logo"><img src="{{ asset('assets/img/logo.png') }}" height="24" alt="logo"></a>
+                            <h5 class="font-size-16 text-white-50 mb-4">OBAC TRAINING CENTER</h5>
+                        </div>
+                    </div>
+                </div>
+                <!-- end row -->
+
+                <div class="row justify-content-center">
+                    <div class="col-lg-5">
+                        <div class="card">
+                            <div class="card-body p-4">
+                                <div class="p-2">
+                                    <h5 class="mb-5 text-center">Se connecter à OTC pour continuer</h5>
+                                    <form class="form-horizontal" action="{{ route('login') }}" method="post">
+
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <div class="form-group mb-4">
+                                                    <label for="username">EMAIL</label>
+                                                    <input type="email" name="email" class="form-control" id="email" placeholder="Saisir votre adresse email">
+                                                </div>
+                                                <div class="form-group mb-4">
+                                                    <label for="password">MOT DE PASSE</label>
+                                                    <input name="password" type="password" class="form-control" id="password" placeholder="Saisir votre mot de passw">
+                                                </div>
+
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        <div class="custom-control custom-checkbox">
+                                                            <input type="checkbox" class="custom-control-input" id="customControlInline">
+                                                            <label class="custom-control-label" for="customControlInline">Se souvenir de moi</label>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="text-md-right mt-3 mt-md-0">
+                                                            <a href="/reset-password" class="text-muted"><i class="mdi mdi-lock"></i> Vous avez oublié votre mot de passe?</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="mt-4">
+                                                    <button class="btn btn-success btn-block waves-effect waves-light" type="submit">Se Connecter</button>
+                                                </div>
+                                                <div class="mt-4 text-center">
+                                                    <a href="/contact" class="text-muted"><i class="mdi mdi-account-circle mr-1"></i> Contacter nous pour avoir un compte</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end row -->
             </div>
-          </div>
-          @if ($errors->has('email'))
-              <span class="help-block">
-                  <strong>Email introuvable</strong>
-              </span>
-          @endif
         </div>
-        <div class="input-group mb-3">
-          <input type="password" name="password" class="form-control" placeholder="Mot de passe">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        <div class="row">
+        <!-- end Account pages -->
 
-          <!-- /.col -->
-          <div class="col-12">
-            <button type="submit" style="border-radius: 5%" class="btn btn-outline-primary btn-block">CONNEXION</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
+        <!-- JAVASCRIPT -->
+        <script src="assets/libs/jquery/jquery.min.js"></script>
+        <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/libs/metismenu/metisMenu.min.js"></script>
+        <script src="assets/libs/simplebar/simplebar.min.js"></script>
+        <script src="assets/libs/node-waves/waves.min.js"></script>
 
-      <div class="social-auth-links text-center mb-3">
-        <p>- OU -</p>
-        <a href="#" class="btn btn-block btn-primary">
-          <i class="fab fa-facebook mr-2"></i> Se connecter par Facebook
-        </a>
+        <script src="assets/js/app.js"></script>
 
-      </div>
-      <!-- /.social-auth-links -->
-
-      <p class="mb-1">
-        <a href="#">J'ai oublié mon mot de passe</a>
-      </p>
-      <p class="mb-0">
-        <a href="/regiter" class="text-center">Créer un nouveau compte</a>
-      </p>
-    </div>
-    <!-- /.login-card-body -->
-  </div>
-</div>
-<!-- /.login-box -->
-
-<!-- jQuery -->
-<script src="<?= asset('plugins/jquery/jquery.min.js') ?>"></script>
-<!-- Bootstrap 4 -->
-<script src="<?= asset('plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
-
-</body>
+    </body>
 </html>
