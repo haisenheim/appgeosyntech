581bf07747db1249f95169785b26de2196aee418
Clement <clementessomba@gmail.com>
2020-02-27 12:52:55 +0100

    Ajouts

--- resources/views/Front/index.blade.php
+++ resources/views/Front/index.blade.php
@@ -251,7 +251,7 @@
                                                         </div>
                                                     </div>
                                                     <div class="overlay-content">
-                                                        <h5 class="font-size-14 text-truncate mb-0">Man Bike Ride on Road</h5>
+                                                        <h5 class="font-size-14 text-truncate mb-0">FORMATION DE RANG <?= $i ?></h5>
                                                     </div>
                                                 </div>
                                             </a>
