f9f5a100cf3e17860063598ccd85999ff663984a
Clement <clementessomba@gmail.com>
2020-03-02 17:15:11 +0100

    Ajouts

--- resources/views/Front/index.blade.php
+++ resources/views/Front/index.blade.php
@@ -151,9 +151,9 @@
                     <div class="popup-gallery">
 
                         @foreach($centres as $centre)
-                            <a class="float-left" href="{{ $centre->imageUri?asset('img/'.$centre->imageUri):'img/logo-obac.png' }}" title="{{ $centre->name }} - <small> {{ $centre->address    . ' - '. $centre->phone }}</small>">
+                            <a class="float-left" href="{{ $centre->imageUri?asset('img/'.$centre->imageUri):'img/logo-obac.png' }}" title="{{ $centre->name }} - <small> {{  $centre->phone }}</small>">
                                 <div class="img-fluid">
-                                    <img src="{{ $centre->imageUri?asset('img/'.$centre->imageUri):'img/logo-obac.png' }}" alt="" width="240">
+                                    <img src="{{ $centre->imageUri?asset('img/'.$centre->imageUri):'img/logo-obac.png' }}" alt="" style="min-height: 240px" width="240">
                                 </div>
                             </a>
                         @endforeach
