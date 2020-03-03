b078a850e0b883774f4b87623d3ff700900f8208
Clement <clementessomba@gmail.com>
2020-03-03 16:41:03 +0100

    Ajouts

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -20,9 +20,9 @@
                         <!-- LOGO -->
                         <div class="navbar-brand-box">
                             <div class="navbar-brand-box">
-                            <a href="/" class="logo logo-dark"><img src="{{ asset('img/logo-obac.png') }}" alt=""/> OBAC TRAINING CENTER</a>
+                            <a href="/" style="color: #FFFFFF" class="logo logo-dark"><img src="{{ asset('img/logo-obac.png') }}" alt=""/> OBAC TRAINING CENTER</a>
 
-                            <a href="/" style="color: #FFFFFF" class="logo logo-light">OBAC TRAINING CENTER</a>
+                            <a href="/" style="color: #FFFFFF" class="logo logo-light"><img src="{{ asset('img/logo-obac.png') }}" alt=""/>OBAC TRAINING CENTER</a>
                         </div>
                         </div>
 
