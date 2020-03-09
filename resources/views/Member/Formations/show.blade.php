7b2980b41b93ba00168e577c733b006fb683908f
Clement <clementessomba@gmail.com>
2020-03-09 17:53:41 +0100

    Ajout du lundi 09 Mars

--- resources/views/Member/Formations/show.blade.php
+++ resources/views/Member/Formations/show.blade.php
@@ -22,8 +22,10 @@
 
             <div class="col-md-9 col-sm-12">
                 <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">LISTE DES MODULES</h4>
+                </div>
                     <div class="card-body">
-                        <h4 class="page-header">LISTE DES MODULES</h4>
 
                              <div class="row">
                                  <div class="col-sm-3">
