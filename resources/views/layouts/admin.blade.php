99ef44d0da762e3f985300b389b7b9e0a7afbf9d
Clement <clementessomba@gmail.com>
2020-03-17 12:00:58 +0100

    Premier commit

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1365,7 +1365,7 @@
         <script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
         <script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
 
-        <!-- apexcharts -->
+        {{--<!-- apexcharts -->
         <script src="{{ asset('assets/libs/apexcharts/apexcharts.min.js') }}"></script>
 
         <script src="{{ asset('assets/libs/slick-slider/slick/slick.min.js') }}"></script>
@@ -1374,7 +1374,7 @@
         <script src="{{ asset('assets/libs/jqvmap/jquery.vmap.min.js') }}"></script>
         <script src="{{ asset('assets/libs/jqvmap/maps/jquery.vmap.usa.js') }}"></script>
 
-        <script src="{{ asset('assets/js/pages/dashboard.init.js') }}"></script>
+        <script src="{{ asset('assets/js/pages/dashboard.init.js') }}"></script>--}}
 
         <script src="{{ asset('assets/js/app.js') }}"></script>
 
