30972cc745214e63b5324d81c75a2e3355b3b5e3
Clement <clementessomba@gmail.com>
2020-03-17 11:45:19 +0100

    Premier commit

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -20,7 +20,7 @@
         <!-- Bootstrap Css -->
         <link href="{{ asset('assets/css/bootstrap.min.css') }} " rel="stylesheet" type="text/css" />
         <!-- Icons Css -->
-        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
         <!-- App Css-->
         <link href="assets/css/app.min.css" rel="stylesheet" type="text/css" />
 
