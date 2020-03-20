b80deffbbb3131a2de45c907ae09bd53c906c626
Clement <clementessomba@gmail.com>
2020-03-20 11:52:41 +0100

    Ajouts

--- app/User.php
+++ app/User.php
@@ -38,53 +38,16 @@ class User extends Authenticatable
         return $this->belongsTo('App\Models\Role');
     }
 
-	public function pays(){
-		return $this->belongsTo('App\Models\Pay','pay_id');
+
+
+	public function client(){
+		return $this->belongsTo('App\Models\Client');
 	}
-
-	public function entreprise(){
-		return $this->belongsTo('App\Models\Entreprise');
-	}
-
-	public function centre(){
-		return $this->belongsTo('App\Models\Centre');
-	}
-
-	public function formations(){
-		return $this->belongsToMany('App\Models\Formation','comptes_formations','compte_id');
-	}
-
-
 
 
     public function getNameAttribute(){
         return $this->last_name . "  ".$this->first_name;
     }
 
-	public function creator(){
-		return $this->belongsTo('App\User','creator_id');
-	}
-
-	public function ville(){
-		return $this->belongsTo('App\User','ville_id');
-	}
-
-	public function getRangAttribute(){
-		$rang = '';
-		if($this->role_id==6){
-			$rang= 'junior';
-
-			if($this->confirmed){
-				$rang= 'confirmé';
-			}
-
-			if($this->senior){
-				$rang= 'senior';
-			}
-
-		}
-
-		return $rang;
-	}
 
 }
