5ca3630da80345986ced38f53dea22f81cdcd3ab
Clement <clementessomba@gmail.com>
2020-03-23 00:43:28 +0100

    ajouts

--- resources/views/Rh/Users/show.blade.php
+++ resources/views/Rh/Users/show.blade.php
@@ -46,6 +46,78 @@
 
                 <div class="card-body">
 
+                    <!-- Nav tabs -->
+                    <ul class="nav nav-pills nav-justified" role="tablist">
+                        <li class="nav-item waves-effect waves-light">
+                            <a class="nav-link active" data-toggle="tab" href="#home-justify" role="tab">
+                                <i class="dripicons-home mr-1 align-middle"></i> <span class="d-none d-md-inline-block">Home</span>
+                            </a>
+                        </li>
+                        <li class="nav-item waves-effect waves-light">
+                            <a class="nav-link" data-toggle="tab" href="#profile-justify" role="tab">
+                                <i class="dripicons-user mr-1 align-middle"></i> <span class="d-none d-md-inline-block">Profile</span>
+                            </a>
+                        </li>
+                        <li class="nav-item waves-effect waves-light">
+                            <a class="nav-link" data-toggle="tab" href="#messages-justify" role="tab">
+                                <i class="dripicons-mail mr-1 align-middle"></i> <span class="d-none d-md-inline-block">Messages</span>
+                            </a>
+                        </li>
+                        <li class="nav-item waves-effect waves-light">
+                            <a class="nav-link" data-toggle="tab" href="#settings-justify" role="tab">
+                                <i class="dripicons-gear mr-1 align-middle"></i> <span class="d-none d-md-inline-block">Settings</span>
+                            </a>
+                        </li>
+                    </ul>
+
+                    <!-- Tab panes -->
+                    <div class="tab-content p-3">
+                        <div class="tab-pane active" id="home-justify" role="tabpanel">
+                            <p class="mb-0">
+                                Etsy mixtape wayfarers, ethical wes anderson tofu before they
+                                sold out mcsweeney's organic lomo retro fanny pack lo-fi
+                                farm-to-table readymade. Messenger bag gentrify pitchfork
+                                tattooed craft beer, iphone skateboard locavore carles etsy
+                                salvia banksy hoodie helvetica. DIY synth PBR banksy irony.
+                                Leggings gentrify squid 8-bit cred pitchfork. Williamsburg banh
+                                mi whatever gluten carles.
+                            </p>
+                        </div>
+                        <div class="tab-pane" id="profile-justify" role="tabpanel">
+                            <p class="mb-0">
+                                Raw denim you probably haven't heard of them jean shorts Austin.
+                                Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache
+                                cliche tempor, williamsburg carles vegan helvetica. Reprehenderit
+                                butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi,
+                                qui irure terry richardson ex squid. Aliquip placeat salvia cillum
+                                iphone. Seitan aliquip quis cardigan american apparel, butcher
+                                voluptate nisi qui.
+                            </p>
+                        </div>
+                        <div class="tab-pane" id="messages-justify" role="tabpanel">
+                            <p class="mb-0">
+                                Food truck fixie locavore, accusamus mcsweeney's marfa nulla
+                                single-origin coffee squid. Exercitation +1 labore velit, blog
+                                sartorial PBR leggings next level wes anderson artisan four loko
+                                farm-to-table craft beer twee. Qui photo booth letterpress,
+                                commodo enim craft beer mlkshk aliquip jean shorts ullamco ad
+                                vinyl cillum PBR. Homo nostrud organic, assumenda labore
+                                aesthetic magna delectus mollit.
+                            </p>
+                        </div>
+                        <div class="tab-pane" id="settings-justify" role="tabpanel">
+                            <p class="mb-0">
+                                Trust fund seitan letterpress, keytar raw denim keffiyeh etsy
+                                art party before they sold out master cleanse gluten-free squid
+                                scenester freegan cosby sweater. Fanny pack portland seitan DIY,
+                                art party locavore wolf cliche high life echo park Austin. Cred
+                                vinyl keffiyeh DIY salvia PBR, banh mi before they sold out
+                                farm-to-table VHS viral locavore cosby sweater. Lomo wolf viral,
+                                mustache.
+                            </p>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
