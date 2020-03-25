bf4e58f3d802ade2511fe2528b786850106d5323
Clement <clementessomba@gmail.com>
2020-03-25 19:32:47 +0100

    ajouts

--- database/seeds/CommandesTableSeeder.php
+++ database/seeds/CommandesTableSeeder.php
@@ -14,20 +14,24 @@ class CommandesTableSeeder extends Seeder
         //
 	    factory(\App\Models\Commande::class,2)->create()->each(function($commande){
 		    $l = rand(2,9);
-		    factory(\App\Models\Cligne::class,$l)->make(['commande_id'=>$commande->id])->create()->each(function($ligne) use($commande){
+		    factory(\App\Models\Cligne::class,$l)->create(['commande_id'=>$commande->id])
+			   // ->make(['commande_id'=>$commande->id])->create()
+			    ->each(function($ligne) use($commande){
 			   $lo =  rand(2,8);
 			    for($i=1;$i<=$ligne->quantity;$i++){
-				    factory(\App\Models\Livraison::class,1)->make(
-					    ['cligne_id'=>$ligne->id,'commande_id'=>$commande->id,'client_id'=>$commande->client_id,'poste_id'=>$ligne->poste_id]
-				    )->create()->each(function($livraison){
+				    factory(\App\Models\Livraison::class,1)->create(['cligne_id'=>$ligne->id,'commande_id'=>$commande->id,'client_id'=>$commande->client_id,'poste_id'=>$ligne->poste_id])
+					    //->make( ['cligne_id'=>$ligne->id,'commande_id'=>$commande->id,'client_id'=>$commande->client_id,'poste_id'=>$ligne->poste_id])->create()
+					    ->each(function($livraison){
 					    $p=rand(1,3);
 					    for($i=1;$i<=$p;$i++){
-						    factory(\App\Models\Prime::class)->make(['livraison_id'=>$livraison->id,'tprime_id'=>$i])->create();
+						    factory(\App\Models\Prime::class)->create(['livraison_id'=>$livraison->id,'tprime_id'=>$i]);
+							    //->make(['livraison_id'=>$livraison->id,'tprime_id'=>$i])->create();
 					    }
 
 					    $j=rand(10,26);
 					    for($i=1;$i<=$j;$i++){
-						    factory(\App\Models\Pointage::class)->make(['livraison_id'=>$livraison->id,'user_id'=>$livraison->user_id])->create();
+						    factory(\App\Models\Pointage::class)->create(['livraison_id'=>$livraison->id,'user_id'=>$livraison->user_id]);
+							    //->make(['livraison_id'=>$livraison->id,'user_id'=>$livraison->user_id])->create();
 					    }
 				    });
 
