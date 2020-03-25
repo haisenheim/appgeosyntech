2726c12f1bcc30027bddeffe8d8a2e271db6018a
Clement <clementessomba@gmail.com>
2020-03-25 16:49:13 +0100

    ajouts

--- resources/views/Rh/Categories/index.blade.php
+++ resources/views/Rh/Categories/index.blade.php
@@ -37,8 +37,9 @@
 
                                <ul class="list-inline " style="margin-left: 10px">
                                     <li class="list-inline-item">{!! $ville->name !!}</li>
+                                    <li class="list-inline-item">{{ number_format($ville->minimum,0,',','.') }}</li>
                                     <li class="list-inline-item"><a title="afficher" class="badge badge-info" href="#"><i class="fa fa-eye"></i></a></li>
-                                    <li class="list-inline-item"><a title="supprimer" class="badge badge-danger" href="#"><i class="fa fa-trash"></i></a></li>
+
                                </ul>
                                </li>
                         @endforeach
