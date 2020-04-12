fcc76bbf54a6592fcc062ab4459b13e84d32085b
Clement <clementessomba@gmail.com>
2020-04-12 17:31:01 +0100

    Ajout des unites de mesures

---
+++ resources/views/Ra/Unites/index.blade.php
@@ -0,0 +1,112 @@
+@extends('......layouts.rh')
+
+
+@section('page-title')
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box d-flex align-items-center justify-content-between">
+                <h4 class="mb-0 font-size-18">UNITES DE MESURE</h4>
+
+                <div class="page-title-right">
+                    <ol class="breadcrumb m-0">
+                        <li class="breadcrumb-item"><a href="javascript: void(0);">SM</a></li>
+                        <li class="breadcrumb-item active">Unités</li>
+                    </ol>
+                </div>
+
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('content')
+
+    <div class="container">
+         <div class="row">
+            <div class="col-12">
+              <div class="card">
+                <div class="card-header">
+                  <h3 class="card-title">UNITES DE MESURE <a class="btn btn-primary btn-xs pull-right" href="#" data-toggle="modal" data-target="#modal-lg"><i class="fa fa-plus-circle"></i></a></h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                    <ul class="list-inline">
+
+                             @foreach($unite as $ville)
+                              <li class="list-inline-item index-item">
+
+                               <ul class="list-inline " style="margin-left: 10px">
+                                    <li class="list-inline-item">{!! $ville->name !!}</li>
+                               </ul>
+                               </li>
+                        @endforeach
+
+                    </ul>
+
+                </div>
+                <!-- /.card-body -->
+              </div>
+
+            </div>
+
+            <!-- /.col -->
+          </div>
+    </div>
+
+
+
+           <div class="modal fade" id="modal-lg">
+                  <div class="modal-dialog modal-sm">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h4 class="modal-title">NOUVELLE UNITE</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div class="modal-body">
+                        <form enctype="multipart/form-data" role="form" action="{{route('ra.unites.store')}}" method="post">
+                        {{csrf_field()}}
+
+                          <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-12 col-sm-12">
+                                    <div class="form-group">
+                                      <label for="name">NOM</label>
+                                      <input type="text" class="form-control" id="name" name="name" placeholder="Saisir le nom ">
+                                    </div>
+                                </div>
+
+
+                            </div>
+
+
+                          </div>
+                          <!-- /.card-body -->
+                          <div class="card-footer">
+                            <button type="submit" class="btn btn-success btn-block"><i class="fa fa-w fa-save"></i> Enregistrer</button>
+                          </div>
+                        </form>
+                      </div>
+
+                    </div>
+                    <!-- /.modal-content -->
+                  </div>
+                  <!-- /.modal-dialog -->
+           </div>
+
+
+<style>
+    .table th,
+    .table td {
+      padding: 0.35rem;
+      vertical-align: top;
+      border-top: 1px solid #dee2e6;
+    }
+  </style>
+
+
+
+
+
+@endsection
