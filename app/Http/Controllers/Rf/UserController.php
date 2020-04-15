b5f9e3501581e8bec105fbdfefdd788749a666f0
Clement <clementessomba@gmail.com>
2020-04-15 23:08:12 +0100

    corrections

---
+++ app/Http/Controllers/Rf/UserController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Rf;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+
+
+
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+	    $users= \App\User::all()->where('role_id',8);
+
+
+        return view('Rf/Users/index')->with(compact('users'));
+
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show($token)
+    {
+        //
+        $user = User::where('token',$token)->first();
+
+        return view('Rf/Users/show')->with(compact('user'));
+    }
+
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Ville  $ville
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Ville $ville)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Ville  $ville
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Ville $ville)
+    {
+        //
+    }
+}
---
+++ resources/views/Rf/Users/index.blade.php
@@ -0,0 +1,103 @@
+
+
+
+@extends('layouts.rf')
+
+@section('page-title')
+    <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">BASE DE DONNEES DES AGENTS</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="/rf/dashboard">TABLEAU DE BORD</a></li>
+              <li class="breadcrumb-item">PARAMETRES</li>
+              <li class="breadcrumb-item active">Agent</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+@endsection
+
+@section('content')
+
+    <div class="row">
+            <div class="col-12">
+              <div class="card">
+                <div class="card-header">
+                  <h3 class="card-title">LISTE DES AGENTS </h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                  <table id="example1" class="table table-bordered table-hover table-condensed datatable">
+                    <thead>
+                    <tr>
+                      <th>NOM</th>
+                      <th>PRENOM</th>
+                      <th>ADRESSE</th>
+                      <th>TELEPHONE</th>
+                      <th>EMAIL</th>
+                      <th>PAYS DE RES.</th>
+                      <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($users as $ville)
+                          <tr>
+                              <td>{!! $ville->last_name !!} </td>
+                              <td>{!! $ville->first_name !!} </td>
+                              <td>{!! $ville->address !!} </td>
+                               <td>{!! $ville->phone !!} </td>
+                                <td>{!! $ville->email !!} </td>
+                              <td>{!! $ville->pay?$ville->pay->name:'-' !!}</td>
+                              <td>
+                              <ul style="margin-bottom: 0" class="list-inline">
+                                <li class="list-inline-item"><a class="btn btn-default btn-xs" href="{{route('rf.users.show',[$ville->token])}}"><i class="fa fa-search"></i></a></li>
+                              </ul>
+                              </td>
+                          </tr>
+                      @endforeach
+
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                      <th>NOM</th>
+                      <th>PRENOM</th>
+                      <th>ADRESSE</th>
+                      <th>TELEPHONE</th>
+                      <th>EMAIL</th>
+                      <th>PAYS RES.</th>
+                      <th></th>
+                    </tr>
+                    </tfoot>
+                  </table>
+                </div>
+                <!-- /.card-body -->
+              </div>
+
+            </div>
+
+            <!-- /.col -->
+          </div>
+
+
+
+<style>
+    .table th,
+    .table td {
+      padding: 0.35rem;
+      vertical-align: top;
+      border-top: 1px solid #dee2e6;
+    }
+  </style>
+
+
+
+@endsection
+
+@section('scripts')
+<script>
+     $(document).ready(function(){$(".datatable").DataTable();});
+</script>
+@endsection
