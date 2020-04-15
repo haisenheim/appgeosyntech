4897bcfe02979cc7705382d270a5e19e9bcfa158
Clement <clementessomba@gmail.com>
2020-04-15 22:52:33 +0100

    corrections

--- resources/views/Rf/Paiements/index.blade.php
+++ resources/views/Rf/Paiements/index.blade.php
@@ -7,7 +7,7 @@
     <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0 text-dark">JOURNAL DE PAIEMENTS</h1>
+            <h2 class="m-0 text-dark">JOURNAL DES PAIEMENTS</h2>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
