cb37f273e3503fe9f53be4cf13caa9b658a9d815
Clement <clementessomba@gmail.com>
2020-04-23 00:39:23 +0100

    Types de contrat

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -32,7 +32,7 @@ class DashboardController extends Controller
 			return $value->contract?($value->contract->tcontract_id == 1):false;
 		})->count();
 		$nb_cdi = $agents->filter(function($value, $key){
-			return $value->contract?($value->contract->tcontract_id == 2):false;
+			return $value->contract?true:false;
 		})->count();
 		$nb_cdp = $agents->filter(function($value, $key){
 			return $value->contract?($value->contract->tcontract_id == 3):false;
